783cfc69ba42606ce13affab1c897b0cca03ba72
pgagarinov <pgagarinov@gmail.com>
2012-10-17 12:16:54 +0000

    Issue 5 Enhancement: make elltool.test.run_tests use new method of test_result BugFix: ellipsoid toolbox configuration is now performed AFTER setting up the path
    git-svn-id: https://ellipsoids.googlecode.com/svn/trunk@78 fbfa2205-8925-0410-8e01-63a555b7045b

--- install/s_install.m
+++ install/s_install.m
@@ -1,10 +1,9 @@
-SEllOptions = ellipsoids_init();
-welcomeString=sprintf('Installing Ellipsoidal Toolbox version %s',...
-    SEllOptions.version);
+welcomeString=sprintf('Installing Ellipsoidal Toolbox');
 disp([welcomeString,'...']);
 s_setpath;
 s_setjavapath;
 %
 modgen.deployment.s_setjavapath
 modgen.logging.log4j.Log4jConfigurator.configureSimply();
+ellipsoids_init();
 disp([welcomeString,': done']);
