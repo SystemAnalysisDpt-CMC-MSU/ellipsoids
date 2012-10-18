625d143d7d16a18b8668820746f228a4cb12f9d8
pgagarinov <pgagarinov@gmail.com>
2012-10-18 19:29:45 +0000

    Issue 5 BugFix: in lib_run_tests called a non-existing function
    git-svn-id: https://ellipsoids.googlecode.com/svn/trunk@92 fbfa2205-8925-0410-8e01-63a555b7045b

--- lib/lib_run_tests.m
+++ lib/lib_run_tests.m
@@ -3,5 +3,3 @@ resList{1}=modgen.test.run_tests(varargin{:});
 resList{2}=mlunitext.test.run_tests(varargin{:});
 %
 results=[resList{:}];
-[errorCount,failCount]=mlunitext.countfailsinresults(results);
-sprintf('(failures: %d, errors %d)',failCount,errorCount);
