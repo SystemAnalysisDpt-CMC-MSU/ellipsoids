3214b373fdeef719d3beb31efa493e5f8a21767c
vitbaranov <vetbar42@gmail.com>
2012-11-01 13:43:53 +0000

    Issue 31. Added high dimensional tests for point-ellipsoid, ellipsoid-ellipsoid and hyperplane-ellipsoids distances, as well as tests with ellipsoids with matrices that have nonzero non diagonal elements, which are obtained by rotating diagonal matrices, multiplying them by random orthogonal matrices.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_31_vbaranov@237 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+core/+test/+mlunit/EllipsoidTestCase.m
+++ products/+elltool/+core/+test/+mlunit/EllipsoidTestCase.m
@@ -13,6 +13,9 @@ classdef EllipsoidTestCase < mlunitext.test_case
         function self = testDistance(self)
             
             global  ellOptions;
+            load(strcat(self.testDataRootDir, '/testEllEllRMat.mat'),...
+                 'testOrth50Mat','testOrth100Mat','testOrth3Mat','testOrth2Mat');
+            %
             %testing vector-ellipsoid distance
             %
             %distance between ellipsoid and two vectors
@@ -52,14 +55,53 @@ classdef EllipsoidTestCase < mlunitext.test_case
             self.runAndCheckError('distance(testEllipsoid, testPointVec)',...
                 'notSecant');
             %
+            %random ellipsoid matrix, low dimension case
+            nDim=2;
+            testEllMat=diag(1:2);
+            testEllMat=testOrth2Mat*testEllMat*testOrth2Mat.';
+            testEllMat=0.5*(testEllMat+testEllMat.');
+            testEllipsoid=ellipsoid(testEllMat);
+            testPoint=testOrth2Mat*[10;zeros(nDim-1,1)];
+            testRes=distance(testEllipsoid, testPoint);
+            mlunit.assert_equals(1,abs(testRes-9)<ellOptions.abs_tol);
+            %
+            %high dimensional tests with rotated ellipsoids
+            nDim=50;
+            testEllMat=diag(nDim:-1:1);
+            testEllMat=testOrth50Mat*testEllMat*testOrth50Mat.';
+            testEllMat=0.5*(testEllMat+testEllMat.');
+            testEllipsoid=ellipsoid(testEllMat);
+            testPoint=testOrth50Mat*[zeros(nDim-1,1);10];
+            testRes=distance(testEllipsoid, testPoint);
+            mlunit.assert_equals(1,abs(testRes-9)<ellOptions.abs_tol);
+            
+            %distance between two ellipsoids with random matrices and two vectors
+            testEll1Mat=[5,2,0;2,5,0;0,0,1];
+            testEll1Mat=testOrth3Mat*testEll1Mat*testOrth3Mat.';
+            testEll1Mat=0.5*(testEll1Mat+testEll1Mat.');
+            testEll2Mat=[4,0,0;0,9,0;0,0,25];
+            testEll2Mat=testOrth3Mat*testEll2Mat*testOrth3Mat.';
+            testEll2Mat=0.5*(testEll2Mat+testEll2Mat.');
+            testEll2CenterVec=testOrth3Mat*[0;0;5];
+            testEllipsoidVec = [ellipsoid(testEll1Mat),...
+                ellipsoid(testEll2CenterVec,testEll2Mat)];
+            testPointMat = testOrth3Mat*([0,0,5; 0,5,5].');
+            testResVec = distance(testEllipsoidVec, testPointMat);
+            mlunit.assert_equals(1, (abs(testResVec(1)-4)<ellOptions.abs_tol) &&...
+                (abs(testResVec(2)-2)<ellOptions.abs_tol));
+            %
+            %
+            %
+            %
             %Testing ellipsoid-ellipsoid distance
+            %
             %distance between two ellipsoids
             testEllipsoid1 = ellipsoid([25,0;0,9]);
-            testEllipsoid2 = ellipsoid([10,0].',[4,0;0,9]);
+            testEllipsoid2 = ellipsoid([10;0],[4,0;0,9]);
             testRes=distance(testEllipsoid1,testEllipsoid2);
             mlunit.assert_equals(1, (abs(testRes-3)<ellOptions.abs_tol));
             %    
-            testEllipsoid1 = ellipsoid([0 -15 0].',[25,0,0;0,100,0;0,0,9]);
+            testEllipsoid1 = ellipsoid([0,-15,0].',[25,0,0;0,100,0;0,0,9]);
             testEllipsoid2 = ellipsoid([0,7,0].',[9,0,0;0,25,0;0,0,100]);
             testRes=distance(testEllipsoid1,testEllipsoid2);
             mlunit.assert_equals(1, (abs(testRes-7)<ellOptions.abs_tol));
@@ -79,7 +121,7 @@ classdef EllipsoidTestCase < mlunitext.test_case
             mlunit.assert_equals(1, (abs(testResVec(1)-8)<ellOptions.abs_tol) &&...
                 (abs(testResVec(2)-5)<ellOptions.abs_tol));
             %            
-            % distance between a of ellipsoids and an ellipsoid 
+            % distance between two ellipsoids and an ellipsoid 
             testEllipsoidVec=[ellipsoid([0, 0, 0].',[9,0,0; 0,25,0; 0,0, 1]),...
                 ellipsoid([-5,0,0].',[9,0,0; 0, 25,0; 0,0,1])];
             testEllipsoid=ellipsoid([5, 0, 0].',[25,0,0; 0,100,0; 0,0, 1]);
@@ -94,12 +136,95 @@ classdef EllipsoidTestCase < mlunitext.test_case
             testRes=distance(testEllipsoid1,testEllipsoid2);
             mlunit.assert_equals(1,abs(testRes-3)<ellOptions.abs_tol);
             %
-            %distance between two vectors of ellipsoids of high dimensions
+            %distance between two vectors of ellipsoids of rather high
+            %dimension (12<=nDim<=26)
             load(strcat(self.testDataRootDir, '/testEllEllDist.mat'),...
                  'testEllipsoid1Vec','testEllipsoid2Vec','testAnswVec','nEllVec');
             testResVec=distance(testEllipsoid1Vec,testEllipsoid2Vec);
             mlunit.assert_equals(ones(1,nEllVec),...
                 abs(testResVec-testAnswVec)<ellOptions.abs_tol);
+            %
+            %distance between two ellipsoids and an ellipsoid (of 3-dimension), 
+            %all matrices with nonzero nondiagonal elements 
+            testEll1Mat=[9,0,0; 0,25,0; 0,0, 1];
+            testEll1Mat=testOrth3Mat*testEll1Mat*testOrth3Mat.';
+            testEll1Mat=0.5*(testEll1Mat+testEll1Mat.');
+            testEll2Mat=[9,0,0; 0, 25,0; 0,0,1];
+            testEll2Mat=testOrth3Mat*testEll2Mat*testOrth3Mat.';
+            testEll2Mat=0.5*(testEll2Mat+testEll2Mat.');
+            testEll2CenterVec=testOrth3Mat*[-5;0;0];
+            testEll3Mat=[25,0,0; 0,100,0; 0,0, 1];
+            testEll3Mat=testOrth3Mat*testEll3Mat*testOrth3Mat.';
+            testEll3Mat=0.5*(testEll3Mat+testEll3Mat.');
+            testEll3CenterVec=testOrth3Mat*[5;0;0];
+            testEllipsoidVec=[ellipsoid(testEll1Mat),...
+                ellipsoid(testEll2CenterVec,testEll2Mat)];
+            testEllipsoid=ellipsoid(testEll3CenterVec,testEll3Mat);
+            testResVec=distance(testEllipsoidVec,testEllipsoid);
+            mlunit.assert_equals(1, (abs(testResVec(1))<ellOptions.abs_tol) &&...
+                (abs(testResVec(2)-2)<ellOptions.abs_tol));
+            %
+            %distance between two ellipsoids of high dimensions and random
+            %matrices
+            nDim=100;
+            testEll1Mat=diag(1:2:2*nDim);
+            testEll1Mat=testOrth100Mat*testEll1Mat*testOrth100Mat.';
+            testEll1Mat=0.5*(testEll1Mat+testEll1Mat.');
+            testEll2Mat=diag([25;(1:(nDim-1)).']);
+            testEll2Mat=testOrth100Mat*testEll2Mat*testOrth100Mat.';
+            testEll2Mat=0.5*(testEll2Mat+testEll2Mat.');
+            testEll2CenterVec=testOrth100Mat*[9;zeros(nDim-1,1)];            
+            testEllipsoid1=ellipsoid(testEll1Mat);
+            testEllipsoid2=ellipsoid(testEll2CenterVec,testEll2Mat);
+            testRes=distance(testEllipsoid1,testEllipsoid2);
+            mlunit.assert_equals(1,abs(testRes-3)<ellOptions.abs_tol);
+            %
+            %
+            %
+            %distance between an ellipsoid (with nonzeros nondiagonal elements)
+            %and a hyperplane in 2 dimensions
+            testEllMat=[9 0; 0 4];
+            testEllMat=testOrth2Mat*testEllMat*testOrth2Mat.';
+            testEllMat=0.5*(testEllMat+testEllMat.');
+            testEllCenterVec=testOrth2Mat*[0;5];
+            testHypVVec=testOrth2Mat*[0;1];
+            testHypC=0;
+            testEllipsoid=ellipsoid(testEllCenterVec,testEllMat);
+            testHyp=hyperplane(testHypVVec,testHypC);
+            testRes=distance(testEllipsoid,testHyp);
+            mlunit.assert_equals(1,abs(testRes-3)<ellOptions.abs_tol);
+            %
+            %distance between an ellipsoid (with nonzero nondiagonal elements)
+            %and a hyperplane in 3 dimensions
+            testEllMat=[100,0,0;0,25,0;0,0,9];
+            testEllMat=testOrth3Mat*testEllMat*testOrth3Mat.';
+            testEllMat=0.5*(testEllMat+testEllMat.');
+            testHypVVec=testOrth3Mat*[0;1;0];
+            testHypC=10;
+            testEllipsoid=ellipsoid(testEllMat);
+            testHyp=hyperplane(testHypVVec,testHypC);
+            testRes=distance(testEllipsoid,testHyp);
+            mlunit.assert_equals(1,abs(testRes-5)<ellOptions.abs_tol);
+            %
+            %distance between two high dimensional ellipsoids (with nonzero
+            %nondiagonal elements) and a hyperplane
+            nDim=100;
+            testEll1Mat=diag(1:nDim);
+            testEll1Mat=testOrth100Mat*testEll1Mat*testOrth100Mat.';
+            testEll1Mat=0.5*(testEll1Mat+testEll1Mat.');
+            testEll1CenterVec=testOrth100Mat*[-8;zeros(nDim-1,1)];    
+            testEll2Mat=diag([25;(1:(nDim-1)).']);
+            testEll2Mat=testOrth100Mat*testEll2Mat*testOrth100Mat.';
+            testEll2Mat=0.5*(testEll2Mat+testEll2Mat.');
+            testEll2CenterVec=testOrth100Mat*[10;zeros(nDim-1,1)];    
+            testHypVVec=testOrth100Mat*[1;zeros(nDim-1,1)];
+            testHypC=0;
+            testEllipsoid=[ellipsoid(testEll1CenterVec,testEll1Mat),...
+                ellipsoid(testEll2CenterVec,testEll2Mat)];
+            testHyp=hyperplane(testHypVVec,testHypC);
+            testRes=distance(testEllipsoid,testHyp);
+            mlunit.assert_equals(1,abs(testRes(1)-7)<ellOptions.abs_tol&&...
+                abs(testRes(2)-5)<ellOptions.abs_tol);          
         end
     end
 end
