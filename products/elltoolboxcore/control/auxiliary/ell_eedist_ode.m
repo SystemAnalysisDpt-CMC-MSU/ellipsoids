320281354da199c8ce27d5c97b2199dac4de55fb
pgagarinov <pgagarinov@gmail.com>
2012-11-20 08:38:52 +0000

    Issue 5 BugFix: performance degradation caused by introduction of Properties class is eliminated
    git-svn-id: https://ellipsoids.googlecode.com/svn/trunk@436 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/elltoolboxcore/control/auxiliary/ell_eedist_ode.m
+++ products/elltoolboxcore/control/auxiliary/ell_eedist_ode.m
@@ -1,86 +1,89 @@
-function dXdt = ell_eedist_ode(t, X, l0, mydata, n, back)
+function dXdt = ell_eedist_ode(t, X, l0, mydata, n, back,absTol)
 %
 % ELL_EEDIST_ODE - ODE for the shape matrix of the external ellipsoid
 %                  for system with disturbance.
 %
 
-  import elltool.conf.Properties;
+import elltool.conf.Properties;
+if nargin<7
+    absTol=Properties.getAbsTol();
+end
 
-  if back > 0
+if back > 0
     t = -t;
     F = ell_value_extract(mydata.Phi, t, [n n]);
     s = -1;
-  else
-    F = ell_value_extract(mydata.Phinv, t, [n n]);	  
+else
+    F = ell_value_extract(mydata.Phinv, t, [n n]);
     s = 1;
-  end
+end
 
-  I     = eye(n);
-  A     = ell_value_extract(mydata.A, t, [n n]);
-  BPB   = ell_value_extract(mydata.BPB, t, [n n]);
-  GQG   = ell_value_extract(mydata.GQG, t, [n n]);
-  GQGsr = ell_value_extract(mydata.GQGsr, t, [n n]);
-  X     = reshape(X, n, n);
-  Y     = sqrtm(X);
-  Y     = 0.5*(Y + Y);
-  mu    = 0;
-  p1    = sqrt(l0' * F * BPB * F' * l0);
-  p2    = sqrt(l0' * F * X * F' * l0);
+I     = eye(n);
+A     = ell_value_extract(mydata.A, t, [n n]);
+BPB   = ell_value_extract(mydata.BPB, t, [n n]);
+GQG   = ell_value_extract(mydata.GQG, t, [n n]);
+GQGsr = ell_value_extract(mydata.GQGsr, t, [n n]);
+X     = reshape(X, n, n);
+Y     = sqrtm(X);
+Y     = 0.5*(Y + Y);
+mu    = 0;
+p1    = sqrt(l0' * F * BPB * F' * l0);
+p2    = sqrt(l0' * F * X * F' * l0);
 
-  if abs(p1) < Properties.getAbsTol()
-    p1 = Properties.getAbsTol();
-  end
-  if abs(p2) < Properties.getAbsTol()
-    p2 = Properties.getAbsTol();
-  end
+if abs(p1) < absTol
+    p1 = absTol;
+end
+if abs(p2) < absTol
+    p2 = absTol;
+end
 
-  pp1 = p1/p2;
-  pp2 = p2/p1;
+pp1 = p1/p2;
+pp2 = p2/p1;
 
-  %g1  = mu * sqrt(l0' * F * F' * l0);
-  %g2  = sqrt(l0' * F * GQG * F' * l0);
+%g1  = mu * sqrt(l0' * F * F' * l0);
+%g2  = sqrt(l0' * F * GQG * F' * l0);
 
-  %if abs(g2) < Properties.getAbsTol()
-  %  g2 = Properties.getAbsTol();
-  %end
+%if abs(g2) < Properties.getAbsTol()
+%  g2 = Properties.getAbsTol();
+%end
 
-  %gg1 = g1/g2;
+%gg1 = g1/g2;
 
-  %if mu == 0
-  %  gg2 = 0;
-  %elseif abs(g1) < Properties.getAbsTol()
-  %  g1  = Properties.getAbsTol();
-  %  gg2 = mu * mu * (g2/g1);
-  %else
-  %  gg2 = mu * mu * (g2/g1);
-  %end
+%if mu == 0
+%  gg2 = 0;
+%elseif abs(g1) < Properties.getAbsTol()
+%  g1  = Properties.getAbsTol();
+%  gg2 = mu * mu * (g2/g1);
+%else
+%  gg2 = mu * mu * (g2/g1);
+%end
 
-  l1 = Y * F' * l0;
-  l2 = GQGsr * F' * l0;
-  if (norm(l1) < Properties.getAbsTol()) || (norm(l2) < Properties.getAbsTol()) %
+l1 = Y * F' * l0;
+l2 = GQGsr * F' * l0;
+if (norm(l1) < absTol) || (norm(l2) < absTol) %
     S = I;
-  else
+else
     S = ell_valign(l1, l2);
-  end
+end
 
-  Z    = Y * S * GQGsr;
-  %dXdt = A*X + X*A' + (pp1 + gg1)*X + pp2*BPB + gg2*I - Z - Z';
-  dXdt = s*A*X + s*X*A' + pp1*X + pp2*BPB - Z - Z';
-  dXdt = 0.5*(dXdt + dXdt');
-  %mn   = min(eig(dXdt));
+Z    = Y * S * GQGsr;
+%dXdt = A*X + X*A' + (pp1 + gg1)*X + pp2*BPB + gg2*I - Z - Z';
+dXdt = s*A*X + s*X*A' + pp1*X + pp2*BPB - Z - Z';
+dXdt = 0.5*(dXdt + dXdt');
+%mn   = min(eig(dXdt));
 
-  %if mn < 0
-  %  mn = abs(mn);
-  %  g1 = sqrt(l0' * F * F' * l0);
-  %  if abs(g1) < Properties.getAbsTol()
-  %    g1 = Properties.getAbsTol();
-  %  end
-  %  gg   = g2/g1;
-  %  ee   = min(svd(X));
-  %  nu   = (mn + Properties.getAbsTol())/(gg + ee);
-  %  mu   = mu + nu;
-  %  dXdt = dXdt + nu*X + nu*gg*I;
-  %  dXdt = 0.5*(dXdt + dXdt');
-  %end
-  
-  dXdt = reshape(dXdt, n*n, 1);
+%if mn < 0
+%  mn = abs(mn);
+%  g1 = sqrt(l0' * F * F' * l0);
+%  if abs(g1) < Properties.getAbsTol()
+%    g1 = Properties.getAbsTol();
+%  end
+%  gg   = g2/g1;
+%  ee   = min(svd(X));
+%  nu   = (mn + Properties.getAbsTol())/(gg + ee);
+%  mu   = mu + nu;
+%  dXdt = dXdt + nu*X + nu*gg*I;
+%  dXdt = 0.5*(dXdt + dXdt');
+%end
+
+dXdt = reshape(dXdt, n*n, 1);
--- products/elltoolboxcore/control/auxiliary/ell_eesm_ode.m
+++ products/elltoolboxcore/control/auxiliary/ell_eesm_ode.m
@@ -1,38 +1,38 @@
-function dXdt = ell_eesm_ode(t, X, l0, mydata, n, back)
+function dXdt = ell_eesm_ode(t, X, l0, mydata, n, back,absTol)
 %
 % ELL_EESM_ODE - ODE for the shape matrix of the external ellipsoid.
 %
+import elltool.conf.Properties;
+if nargin<7
+    absTol=Properties.getAbsTol();
+end
 
-  import elltool.conf.Properties;
-
-  if back > 0
+if back > 0
     t = -t;
     F = ell_value_extract(mydata.Phi, t, [n n]);
     s = -1;
-  else
-    F = ell_value_extract(mydata.Phinv, t, [n n]);	  
+else
+    F = ell_value_extract(mydata.Phinv, t, [n n]);
     s = 1;
-  end
+end
+%
+A   = ell_value_extract(mydata.A, t, [n n]);
+BPB = ell_value_extract(mydata.BPB, t, [n n]);
+X   = reshape(X, n, n);
 
-  A   = ell_value_extract(mydata.A, t, [n n]);
-  BPB = ell_value_extract(mydata.BPB, t, [n n]);
-  X   = reshape(X, n, n);
+p1 = sqrt(l0' * F * BPB * F' * l0);
+p2 = sqrt(l0' * F * X * F' * l0);
 
-  p1 = sqrt(l0' * F * BPB * F' * l0);
-  p2 = sqrt(l0' * F * X * F' * l0);
+if abs(p1) < absTol
+    p1 = absTol;
+end
+if abs(p2) < absTol
+    p2 = absTol;
+end
 
-  if abs(p1) < Properties.getAbsTol()
-    p1 = Properties.getAbsTol();
-  end
-  if abs(p2) < Properties.getAbsTol()
-    p2 = Properties.getAbsTol();
-  end
+pp1 = p1/p2;
+pp2 = p2/p1;
 
-  pp1 = p1/p2;
-  pp2 = p2/p1;
-  
-  AX = A*X;
-  dXdt = s*AX + s*AX' + pp1*X + pp2*BPB;
-  dXdt = reshape(0.5*(dXdt + dXdt'), n*n, 1);
-
-  return;
+AX = A*X;
+dXdt = s*AX + s*AX' + pp1*X + pp2*BPB;
+dXdt = reshape(0.5*(dXdt + dXdt'), n*n, 1);
