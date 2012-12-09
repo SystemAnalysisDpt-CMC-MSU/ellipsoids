f18bf3d1e13bb27695e0e277ed2e29b189a27d30
pgagarinov <pgagarinov@gmail.com>
2012-12-09 19:51:30 +0000

    Issue 14 All done
    git-svn-id: https://ellipsoids.googlecode.com/svn/trunk@708 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+core/+test/run_tests.m
+++ products/+elltool/+core/+test/run_tests.m
@@ -5,9 +5,11 @@ loader = mlunitext.test_loader;
      'elltool.core.test.mlunit.EllipsoidIntUnionTC',varargin{:});
  suite2 =loader.load_tests_from_test_case(...
      'elltool.core.test.mlunit.EllipsoidTestCase',varargin{:});
- suite3 = loader.load_tests_from_test_case(...
+ suite3 =loader.load_tests_from_test_case(...
+     'elltool.core.test.mlunit.EllipsoidSecTestCase',varargin{:});
+ suite4 = loader.load_tests_from_test_case(...
          'elltool.core.test.mlunit.HyperplaneTestCase', varargin{:});
- suite=mlunit.test_suite(horzcat(suite1.tests,suite2.tests,suite3.tests));
+ suite=mlunit.test_suite(horzcat(suite1.tests,suite2.tests,suite3.tests,suite4.tests));
  resList{1}=runner.run(suite);
  
  suite = loader.load_tests_from_test_case(...
@@ -15,11 +17,15 @@ loader = mlunitext.test_loader;
  resList{2}=runner.run(suite);
  
  suite = loader.load_tests_from_test_case(...
- 'elltool.core.test.mlunit.HyperplaneTestCase',varargin{:});
+ 'elltool.core.test.mlunit.EllipsoidSecTestCase',varargin{:});
  resList{3}=runner.run(suite);
+ 
+ suite = loader.load_tests_from_test_case(...
+ 'elltool.core.test.mlunit.HyperplaneTestCase',varargin{:});
+ resList{4}=runner.run(suite);
 
  suite = loader.load_tests_from_test_case(...
  'elltool.core.test.mlunit.GenEllipsoidTestCase',varargin{:});
- resList{4}=runner.run(suite);
+ resList{5}=runner.run(suite);
 
 result=[resList{:}];
