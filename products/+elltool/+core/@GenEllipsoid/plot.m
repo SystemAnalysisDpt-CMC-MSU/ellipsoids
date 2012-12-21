f2d0a7edfe64f3b8b6afba5a663e136d13867daa
pgagarinov <pgagarinov@gmail.com>
2012-12-21 11:51:49 +0000

    Issue 54 New features: help-headers added. Small bugs fixed
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_54_vkaushanskiy@734 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+core/@GenEllipsoid/plot.m
+++ products/+elltool/+core/@GenEllipsoid/plot.m
@@ -1,5 +1,41 @@
 function plObj = plot(varargin)
+%
+% PLOT - plots General Ellipsoids in 2D or 3D.
+%
+%
+% Description:
+% ------------
+%
+% PLOT(E, 'Property',PropValue) plots ellipsoid E if 2 <= dimension(E) <= 3.
+%
+%                  PLOT(E)  Plots E in default (red) color.
+%              PLOT(EA, E)  Plots array of ellipsoids EA and single ellipsoid E.
+%   PLOT(E1, 'g', E2, 'b')  Plots E1 in green and E2 in blue color.
+%        PLOT(EA, 'Property',PropValue)  Plots EA  by setting properties.
+%
+% Properties:
+% 'newFigure'    - if 1, each plot command will open a new figure window.
+% 'fill'         - if 1, ellipsoids in 2D will be filled with color.
+% 'lineWidth'        - line width for 1D and 2D plots.
+% 'color'        - sets default colors in the form [x y z].
+% 'shade' = 0-1  - level of transparency (0 - transparent, 1 - opaque).
+%  'relDataPlotter' - relation data plotter object
+%
+% Output:
+% -------
+%
+%     plObj - returns the relation data plotter object
+%
 
+%
+% Author:
+% -------
+%
+%    $Author: Vadim Kaushanskiy  <vkaushanskiy@gmail.com> $    $Date: 21-December-2012 $
+%$Copyright: Moscow State University,
+%            Faculty of Computational Mathematics and Cybernetics,
+%            System Analysis Department 2012 $
+%
 import elltool.conf.Properties;
 import modgen.common.throwerror;
 import elltool.core.GenEllipsoid;
@@ -37,16 +73,16 @@ calcEllPoints();
 
 
 rel=smartdb.relations.DynamicRelation(SData);
-if ~ishold
-    isHold = false;
-    if isNewFigure
-        close;
-    else
-        cla;
-    end
+hFigure = get(0,'CurrentFigure');
+if isempty(hFigure)
+  isHold=false;
+elseif ~ishold
+  cla;
+  isHold = false;
 else
     isHold = true;
 end
+
 if (nDim==2)
     plObj.plotGeneric(rel,@figureGetGroupNameFunc,{'figureNameCMat'},...
         @figureSetPropFunc,{},...
