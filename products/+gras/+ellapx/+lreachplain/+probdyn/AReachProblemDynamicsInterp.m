53ebd306bf35d5bc1a2526b36ee25fa99f6d9ff1
pgagarinov <pgagarinov@gmail.com>
2013-03-20 22:14:06 +0000

    issue #72 Enhancement: Draft version of R(t,t0) calculations. Problems with 'basic' tests have been detected.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_72_yadmiralsky@974 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+gras/+ellapx/+lreachplain/+probdyn/AReachProblemDynamicsInterp.m
+++ products/+gras/+ellapx/+lreachplain/+probdyn/AReachProblemDynamicsInterp.m
@@ -2,6 +2,9 @@ classdef AReachProblemDynamicsInterp<...
         gras.ellapx.lreachplain.probdyn.AReachProblemDynamics
     methods
         function self=AReachProblemDynamicsInterp(problemDef,calcPrecision)
+            % that isn't good
+            global global_AtDyn global_sizeAtVec global_numelAt;
+            
             import gras.ellapx.common.*;
             import gras.interp.MatrixInterpolantFactory;
             import gras.ellapx.lreachplain.probdyn.LReachProblemDynamicsInterp;
@@ -36,15 +39,41 @@ classdef AReachProblemDynamicsInterp<...
             odeArgList=self.getOdePropList(calcPrecision);
             solverObj=MatrixODESolver(sizeAtVec,@ode45,odeArgList{:});
             %
-            Xtt0DerivFunc = @(t,x) reshape(self.AtDynamics.evaluate(t)*...
-                reshape(x,sizeAtVec),[numelAt 1]);
-            Xtt0InitialMat = eye(sizeAtVec);
+            % old code will be here for some time
             %
-            [timeXtt0Vec,data_Xtt0]=solverObj.solve(Xtt0DerivFunc,...
-                self.timeVec,Xtt0InitialMat);
+            %Xtt0DerivFunc = @(t,x) reshape(self.AtDynamics.evaluate(t)*...
+                %reshape(x,sizeAtVec),[numelAt 1]);
+            %
+            % set global variables
+            %
+            global_AtDyn = @(t) self.AtDynamics.evaluate(t);
+            global_sizeAtVec = sizeAtVec;
+            global_numelAt = numelAt;
+            %
+            Rtt0DerivFunc = @(t,x) Rtt0DerivFunc_body(t, x);
+            %            
+            Rtt0InitialMat = eye(sizeAtVec);
+            Rtt0InitialMat = Rtt0InitialMat / sqrt(sum(Rtt0InitialMat(:)));
+            %
+            [timeRtt0Vec,data_Rtt0]=solverObj.solve(Rtt0DerivFunc,...
+                self.timeVec,Rtt0InitialMat);
             %
             self.Xtt0Dynamics=MatrixInterpolantFactory.createInstance(...
-                'column',data_Xtt0,timeXtt0Vec);
+                'column',data_Rtt0,timeRtt0Vec);
         end
     end
+end
+
+function dx = Rtt0DerivFunc_body(t, x)
+    global global_AtDyn global_sizeAtVec global_numelAt;
+    %
+    Rtt0Mat = reshape(x, global_sizeAtVec);
+    cachedMat = global_AtDyn(t) * Rtt0Mat;
+    %
+    dRtt0Mat = cachedMat - Rtt0Mat * sum(Rtt0Mat(:) .* cachedMat(:));
+    dx = reshape(dRtt0Mat, [global_numelAt 1]);
+    
+    %Xtt0 = reshape(x, global_sizeAtVec);
+    %dXtt0 = global_AtDyn(t) * Xtt0;
+    %dx = reshape(dXtt0, [global_numelAt 1]);
 end
