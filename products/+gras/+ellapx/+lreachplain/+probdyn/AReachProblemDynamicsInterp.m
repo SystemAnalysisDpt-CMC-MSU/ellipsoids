63333c2ef6734af5bb05a462de5cc264a3dc6fa7
pgagarinov <pgagarinov@gmail.com>
2013-03-23 20:03:12 +0000

    issue #72 Bugfix: global variables have been deleted.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_72_yadmiralsky@1001 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+gras/+ellapx/+lreachplain/+probdyn/AReachProblemDynamicsInterp.m
+++ products/+gras/+ellapx/+lreachplain/+probdyn/AReachProblemDynamicsInterp.m
@@ -1,10 +1,11 @@
 classdef AReachProblemDynamicsInterp<...
         gras.ellapx.lreachplain.probdyn.AReachProblemDynamics
+    properties (Access = private)
+        sizeAtVec = [0 0];
+        numelAt = 0;
+    end
     methods
-        function self=AReachProblemDynamicsInterp(problemDef,calcPrecision)
-            % that isn't good
-            global global_AtDyn global_sizeAtVec global_numelAt;
-            
+        function self=AReachProblemDynamicsInterp(problemDef,calcPrecision)          
             import gras.ellapx.common.*;
             import gras.interp.MatrixInterpolantFactory;
             import gras.ellapx.lreachplain.probdyn.LReachProblemDynamicsInterp;
@@ -18,8 +19,8 @@ classdef AReachProblemDynamicsInterp<...
             AtDefCMat = problemDef.getAMatDef();
             t0 = problemDef.gett0();
             t1 = problemDef.gett1();
-            sizeAtVec = size(AtDefCMat);
-            numelAt = numel(AtDefCMat);
+            self.sizeAtVec = size(AtDefCMat);
+            self.numelAt = numel(AtDefCMat);
             %
             self.timeVec = linspace(t0,t1,self.N_TIME_POINTS);
             %
@@ -37,43 +38,65 @@ classdef AReachProblemDynamicsInterp<...
             % compute X(t,t0)
             %
             odeArgList=self.getOdePropList(calcPrecision);
-            solverObj=MatrixODESolver(sizeAtVec,@ode45,odeArgList{:});
+            solverObj=MatrixODESolver(self.sizeAtVec,@ode45,odeArgList{:});
             %
             % old code will be here for some time
             %
             %Xtt0DerivFunc = @(t,x) reshape(self.AtDynamics.evaluate(t)*...
                 %reshape(x,sizeAtVec),[numelAt 1]);
             %
-            % set global variables
-            %
-            global_AtDyn = @(t) self.AtDynamics.evaluate(t);
-            global_sizeAtVec = sizeAtVec;
-            global_numelAt = numelAt;
-            %
-            Rtt0DerivFunc = @(t,x) Rtt0DerivFunc_body(t, x);
+            Rtt0DerivFunc = @(t,x) self.Rtt0DerivFunc_body(t, x);
             %            
-            Rtt0InitialMat = eye(sizeAtVec);
-            Rtt0InitialMat = Rtt0InitialMat / sqrt(sum(Rtt0InitialMat(:)));
+            Rtt0InitialMat = eye(self.sizeAtVec);
+            Rtt0InitialMat = normaliz(Rtt0InitialMat);
             %
             [timeRtt0Vec,data_Rtt0]=solverObj.solve(Rtt0DerivFunc,...
                 self.timeVec,Rtt0InitialMat);
+            %
+            % the code here is control code (just calculates X(t, t0) and
+            % then divide it by matrixnorm(X(t,t0))
+            %
+            %data_Rtt0 = normaliz(data_Rtt0);
             %
             self.Xtt0Dynamics=MatrixInterpolantFactory.createInstance(...
                 'column',data_Rtt0,timeRtt0Vec);
         end
     end
+    
+    methods (Access = private)
+        function dx = Rtt0DerivFunc_body(dyn_interp, t, x)
+            %
+            Rtt0Mat = reshape(x, dyn_interp.sizeAtVec);
+            cachedMat = dyn_interp.AtDynamics.evaluate(t) * Rtt0Mat;
+            %
+            dRtt0Mat = cachedMat - Rtt0Mat * sum(Rtt0Mat(:) .* cachedMat(:));
+            dx = reshape(dRtt0Mat, [dyn_interp.numelAt 1]);
+            %
+        end
+        function dx = Xtt0DerivFunc_body(dyn_interp, t, x)
+            %
+            Xtt0 = reshape(x, dyn_interp.sizeAtVec);
+            dXtt0 = dyn_interp.AtDynamics.evaluate(t) * Xtt0;
+            dx = reshape(dXtt0, [dyn_interp.numelAt 1]);
+            %
+        end
+    end
 end
 
-function dx = Rtt0DerivFunc_body(t, x)
-    global global_AtDyn global_sizeAtVec global_numelAt;
-    %
-    Rtt0Mat = reshape(x, global_sizeAtVec);
-    cachedMat = global_AtDyn(t) * Rtt0Mat;
-    %
-    dRtt0Mat = cachedMat - Rtt0Mat * sum(Rtt0Mat(:) .* cachedMat(:));
-    dx = reshape(dRtt0Mat, [global_numelAt 1]);
-    
-    %Xtt0 = reshape(x, global_sizeAtVec);
-    %dXtt0 = global_AtDyn(t) * Xtt0;
-    %dx = reshape(dXtt0, [global_numelAt 1]);
+function nA = normaliz(A)
+    szVec = size(A);
+    normMat = A .* A;
+    normMat = sum(normMat, 2);
+    normMat = sum(normMat, 1);
+    normMat = sqrt(normMat);
+    switch (length(szVec))
+        case 2
+            nA = A / normMat;
+        case 3
+            normMat = repmat(normMat, [szVec(1), szVec(2), 1]);
+            nA = A ./ normMat;
+    end
 end
+
+
+
