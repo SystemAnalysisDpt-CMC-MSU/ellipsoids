6f9391a6a4af6dc4be1f17815da2cf462039faa2
pgagarinov <pgagarinov@gmail.com>
2013-03-26 17:05:35 +0000

    issue #72 Enhancement: added some code for testing.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_72_yadmiralsky@1046 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+gras/+ellapx/+lreachplain/+probdyn/AReachProblemDynamicsInterp.m
+++ products/+gras/+ellapx/+lreachplain/+probdyn/AReachProblemDynamicsInterp.m
@@ -40,12 +40,37 @@ classdef AReachProblemDynamicsInterp<...
             odeArgList=self.getOdePropList(calcPrecision);
             solverObj=MatrixODESolver(self.sizeAtVec,@ode45,odeArgList{:});
             %
+            % (temporary) setting algorithm:
+            %   0 - X(t, t0) / matrixnorm(X(t, t0))
+            %   1 - R(t, t0)
+            %   2 - [R(t, t0); matrixnorm(X(t, t0))]
+            %
+            algorithm = 2;            
+            %
             % we can just change Rtt0DerivFunc_body to Xtt0DerivFunc_body
             %
-            Rtt0DerivFunc = @(t,x) self.Rtt0DerivFunc_body(t, x);
+            switch (algorithm)
+                case 0
+                    Rtt0DerivFunc = @(t,x) self.Xtt0DerivFunc_body(t, x);
+                case 1
+                    Rtt0DerivFunc = @(t,x) self.Rtt0DerivFunc_body(t, x);
+                case 2
+                    Rtt0DerivFunc = @(t,x) self.Rtt0ExtDerivFunc_body(t, x);
+            end
             %
-            Rtt0InitialMat = eye(self.sizeAtVec);
-            Rtt0InitialMat = normaliz(Rtt0InitialMat);
+            switch (algorithm)
+                case 0
+                    Rtt0InitialMat = eye(self.sizeAtVec);
+                case 1
+                    Rtt0InitialMat = eye(self.sizeAtVec);
+                    Rtt0InitialMat = normaliz(Rtt0InitialMat);
+                case 2
+                    Rtt0InitialMat = eye(self.sizeAtVec);
+                    norm = sqrt(sum(sum(Rtt0InitialMat, 2)));
+                    Rtt0InitialMat = normaliz(Rtt0InitialMat);
+                    Rtt0InitialMat = [Rtt0InitialMat(:); norm];
+            end
+            
             %
             [timeRtt0Vec,data_Rtt0]=solverObj.solve(Rtt0DerivFunc,...
                 self.timeVec,Rtt0InitialMat);
@@ -53,7 +78,15 @@ classdef AReachProblemDynamicsInterp<...
             % the code here is control code (just calculates X(t, t0) and
             % then divide it by matrixnorm(X(t,t0))
             %
-            %data_Rtt0 = normaliz(data_Rtt0);
+            switch (algorithm)
+                case 0
+                    data_Rtt0 = normaliz(data_Rtt0);
+                case 2
+                    sz = size(data_Rtt0);
+                    normVec = data_Rtt0(sz(1), :);
+                    data_Rtt0(sz(1), :) = [];
+                    data_Rtt0 = reshape(data_Rtt0, [dyn_interp.sizeAtVec, dyn_interp.sizeAtVec, self.N_TIME_POINTS]);
+            end
             %
             self.Xtt0Dynamics=MatrixInterpolantFactory.createInstance(...
                 'column',data_Rtt0,timeRtt0Vec);
@@ -61,6 +94,22 @@ classdef AReachProblemDynamicsInterp<...
     end
     
     methods (Access = private)
+        %
+        % new equation for R(t, t0) and norm(X(t, t0))(t)
+        %
+        function dx = Rtt0ExtDerivFunc_body(dyn_interp, t, x)
+            norm = x(length(x)); x(length(x)) = [];
+            Rtt0Mat = reshape(x, dyn_interp.sizeAtVec);
+            %
+            cachedMat = dyn_interp.AtDynamics.evaluate(t) * Rtt0Mat;
+            %
+            dnorm = sum(Rtt0Mat(:) .* cachedMat(:)) * norm;
+            dRtt0Mat = cachedMat - Rtt0Mat * sum(Rtt0Mat(:) .* cachedMat(:));
+            dx = reshape(dRtt0Mat, [dyn_interp.numelAt 1]);
+            %
+            dx = [dx; dnorm];
+            %
+        end
         %
         % new equation for R(t, t0)
         %
