2aaefcdea9871bcb3ac18d246dc5501360fb8c82
vitbaranov <vetbar42@gmail.com>
2013-03-26 05:12:50 +0000

    Issue 39. Enhancement: completed linsys and reach classes.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_39_vbaranov@1023 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+linsys/LinSys.m
+++ products/+elltool/+linsys/LinSys.m
@@ -37,7 +37,7 @@ classdef LinSys < handle
     end
     %
     methods (Access = private, Static)
-        function isEllHaveNeededDim(InpEll, nDim)
+        function isEllHaveNeededDim(InpEll, nDim, absTol)
         % isEllHaveNeededDim - checks if given structure InpEll represents
         %     an ellipsoid of dimension nDim.
         %
@@ -46,6 +46,8 @@ classdef LinSys < handle
         %       InpEll: struct[1, 1]
         %
         %       nDim: double[1, 1]
+        %
+        %       absTol: doulbe[1,1]
         %
         % Output:
         %   None.
@@ -58,7 +60,7 @@ classdef LinSys < handle
             %%
             if mRows ~= nCols
                 throwerror(sprintf('value:%s:shape', inputname(1)),...
-                    'shape matrix must be symmetric, positive definite');
+                    'shape matrix must be square');
             elseif nCols ~= nDim
                 throwerror(sprintf('linsys:dimension:%s:shape', inputname(1)),...
                     'shape matrix must be of dimension %dx%d', nDim, nDim);
@@ -88,10 +90,14 @@ classdef LinSys < handle
                 end
             else
                 if isa(QMat, 'double')
-                    isnEqMat = (QMat ~= QMat.');
-                    if any(isnEqMat(:)) || min(eig(QMat)) <= 0
-                        throwerror(sprintf('linsys:value:%s:shape', inputname(1)),...
-                            'shape matrix must be symmetric, positive definite');
+                    if ~gras.la.ismatsymm(QMat)
+                        throwerror(sprintf('linsys:value:%s:shape', ...
+                            inputname(1)),...
+                            'shape matrix must be symmetric');
+                    elseif ~gras.la.ismatposdef(QMat,absTol,false)
+                        throwerror(sprintf('linsys:value:%s:shape',...
+                            inputname(1)),...
+                            'shape matrix must be  positive definite');
                     end                    
                 else
                     throwerror(sprintf('type:%s:shape', inputname(1)),...
@@ -219,6 +225,7 @@ classdef LinSys < handle
                 self.absTol = absTolVal;
                 return;
             end
+            self.absTol = absTolVal;
             %%
             isTimeInvar = true;
             [mRows, nCols] = size(atInpMat);
@@ -279,7 +286,7 @@ classdef LinSys < handle
                         isfield(uBoundsEll, 'shape')
                     isCBU = false;
                     uBoundsEll = uBoundsEll(1, 1);
-                    self.isEllHaveNeededDim(uBoundsEll, lCols);      
+                    self.isEllHaveNeededDim(uBoundsEll, lCols,self.absTol);      
                 else
                     throwerror('linsys:type:U',...
                         'LINSYS: control U must be an ellipsoid or a vector.')
@@ -338,7 +345,8 @@ classdef LinSys < handle
                         isfield(distBoundsEll, 'shape')
                     isCBV = false;
                     distBoundsEll = distBoundsEll(1, 1);
-                    self.isEllHaveNeededDim(distBoundsEll, lCols);
+                        self.isEllHaveNeededDim(distBoundsEll, lCols,...
+                        self.absTol);
                 else
                     throwerror('linsys:type:V',...
                         'LINSYS: disturbance V must be an ellipsoid or a vector.')
@@ -398,7 +406,8 @@ classdef LinSys < handle
                         isfield(noiseBoundsEll, 'shape')
                     isCBW = false;
                     noiseBoundsEll = noiseBoundsEll(1, 1);
-                    self.isEllHaveNeededDim(noiseBoundsEll, kRows);
+                    self.isEllHaveNeededDim(noiseBoundsEll, kRows, ...
+                        self.absTol);
                 else
                     throwerror('linsys:type:W',...
                         'LINSYS: noise W must be an ellipsoid or a vector.')
@@ -414,7 +423,6 @@ classdef LinSys < handle
                 self.isDiscr = true;
             end
             self.isConstantBoundsVec = [isCBU isCBV isCBW];
-            self.absTol = absTolVal;
         end
         %
         function [stateDimArr, inpDimArr, outDimArr, distDimArr] =...
