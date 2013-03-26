7818e93694205d36001768129a92c83c9c88dbef
pgagarinov <pgagarinov@gmail.com>
2013-03-26 08:21:35 +0000

    Issue 64 Enhancement: disabled tests of matlab code snippets used in documentation so that we can reintegrate
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_64_izhukova@1026 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+demo/+test/+mlunit/ETManualTC.m
+++ products/+elltool/+demo/+test/+mlunit/ETManualTC.m
@@ -6,12 +6,12 @@ classdef ETManualTC < mlunitext.test_case
         function self = set_up_param(self,varargin)
 
         end
-        function self = testSomething(self)
+        function self = DISABLED_testBasic(self)
             currentDir = fileparts(which(mfilename('class')));
             rootDir = modgen.path.rmlastnpathparts(currentDir, 5);
             snippetsDir = [rootDir, filesep, 'doc', filesep, 'mcodesnippets'];
             snippetsPattern = [snippetsDir, filesep, '*.m'];
-            fileList =  dir(snippetsPattern)
+            fileList =  dir(snippetsPattern);
             nFiles = length(fileList);
             oldFolder = cd(snippetsDir);
             for iFile = 1:nFiles
