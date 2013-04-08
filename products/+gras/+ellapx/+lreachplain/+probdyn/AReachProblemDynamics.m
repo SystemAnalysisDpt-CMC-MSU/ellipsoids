1411b6f5a20227fabec7435e78d59f2f07e24b63
pgagarinov <pgagarinov@gmail.com>
2013-04-08 19:55:14 +0000

    issue #72 Enhancement: all new R(t, t0) calculation methods from now on will be in 'AReachProblemDynamics' superclass.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_72_yadmiralsky@1202 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+gras/+ellapx/+lreachplain/+probdyn/AReachProblemDynamics.m
+++ products/+gras/+ellapx/+lreachplain/+probdyn/AReachProblemDynamics.m
@@ -64,4 +64,135 @@ classdef AReachProblemDynamics<...
             problemDef=self.problemDef;
         end
     end
+    methods (Access = protected)
+        %
+        % method for R(t,t0) calculations
+        %
+        function calcRtt0Dyn(self, sizeAtVec, numelAt, odeArgList)
+            %
+            import gras.interp.MatrixInterpolantFactory;
+            import gras.ode.MatrixODESolver;
+            % (temporary) setting algorithm:
+            %   0 - X(t, t0)
+            %   1 - X(t, t0) / matrixnorm(X(t, t0))
+            %   2 - R(t, t0)
+            %   3 - [R(t, t0); matrixnorm(X(t, t0))]
+            %
+            algorithm = 2;
+            %
+            % the code here is a control code
+            % [realcode]
+            % solverObj=MatrixODESolver(sizeAtVec,@ode45,odeArgList{:});
+            % [end]
+            %
+            if (algorithm == 3)
+                solverObj=MatrixODESolver([numelAt + 1, 1],@ode45,odeArgList{:});
+            else
+                solverObj=MatrixODESolver(sizeAtVec,@ode45,odeArgList{:});
+            end
+            %
+            % the code here is a control code
+            % [realcode]
+            % fRtt0DerivFunc = @(t,x) fRtt0SimDerivFunc(t, x, @(u) self.AtDynamics.evaluate(u));
+            % [end]
+            %
+            switch (algorithm)
+                case 0
+                    fRtt0DerivFunc = @(t,x) fXtt0DerivFunc(t, x, @(u) self.AtDynamics.evaluate(u));
+                case 1
+                    fRtt0DerivFunc = @(t,x) fXtt0DerivFunc(t, x, @(u) self.AtDynamics.evaluate(u));
+                case 2
+                    fRtt0DerivFunc = @(t,x) fRtt0SimDerivFunc(t, x, @(u) self.AtDynamics.evaluate(u));
+                case 3
+                    fRtt0DerivFunc = @(t,x) fRtt0ExtDerivFunc(t, x, @(u) self.AtDynamics.evaluate(u), sizeAtVec);
+            end
+            %
+            % the code here is a control code
+            % [realcode]
+            % sRtt0InitialMat = eye(sizeAtVec);
+            % sRtt0InitialMat = normaliz(sRtt0InitialMat);
+            % [end]
+            %
+            switch (algorithm)
+                case 0
+                    sRtt0InitialMat = eye(sizeAtVec);
+                case 1
+                    sRtt0InitialMat = eye(sizeAtVec);
+                case 2
+                    sRtt0InitialMat = eye(sizeAtVec);
+                    sRtt0InitialMat = normaliz(sRtt0InitialMat);
+                case 3
+                    sRtt0InitialMat = eye(sizeAtVec);
+                    norm = sqrt(sum(sum(sRtt0InitialMat, 2)));
+                    sRtt0InitialMat = normaliz(sRtt0InitialMat);
+                    sRtt0InitialMat = [sRtt0InitialMat(:); norm];
+            end
+            %
+            [timeRtt0Vec,dataRtt0Array]=solverObj.solve(fRtt0DerivFunc,...
+                self.timeVec,sRtt0InitialMat);
+            %
+            % the code here is a control code
+            % [realcode]
+            % [end]
+            %
+            switch (algorithm)
+                case 1
+                    dataRtt0Array = normaliz(dataRtt0Array);
+                case 3
+                    sz = size(dataRtt0Array);
+                    normVec = dataRtt0Array(sz(1), sz(2), :);
+                    normVec = repmat(normVec, [sizeAtVec, 1]);
+                    dataRtt0Array = dataRtt0Array(1:(sz(1) - 1), :, :);
+                    dataRtt0Array = reshape(dataRtt0Array, [sizeAtVec, self.N_TIME_POINTS]);
+                    dataRtt0Array = dataRtt0Array .* normVec;
+            end
+            %
+            self.Rtt0Dynamics=MatrixInterpolantFactory.createInstance(...
+                'column',dataRtt0Array,timeRtt0Vec);
+        end
+    end
+end
+
+%
+% new equation for R(t, t0) and matrixnorm(X(t, t0))
+%
+function dx = fRtt0ExtDerivFunc(t, x, fAt, sizeAtVec)
+    norm = x(length(x)); x(length(x)) = [];
+    sRtt0Mat = reshape(x, sizeAtVec);
+    %
+    cachedMat = fAt(t) * sRtt0Mat;
+    scprod = sum(sRtt0Mat(:) .* cachedMat(:));
+    %
+    dnorm = scprod * norm;
+    dsRtt0Mat = cachedMat - sRtt0Mat * scprod;
+    %
+    dx = [dsRtt0Mat(:); dnorm];
+end
+%
+% new equation for R(t, t0)
+%
+function dx = fRtt0SimDerivFunc(t, x, fAt)
+    cachedMat = fAt(t) * x;
+    %
+    dx = cachedMat - x * sum(x(:) .* cachedMat(:));
+end
+%
+% old equation for X(t, t0)
+%
+function dx = fXtt0DerivFunc(t, x, fAt)
+    dx = fAt(t) * x;
+end
+
+%
+% normalizes matrix argMat (matrixnorm(normalizMat) = 1)
+%
+function normalizMat = normaliz(argMat)
+    szVec = size(argMat);
+    normMat = argMat .* argMat;
+    normMat = sqrt(sum(sum(normMat, 2), 1));
+    if (length(szVec) > 2)
+        szVec(3:length(szVec)) = 1;
+    end
+    normMat = repmat(normMat, szVec);
+    normalizMat = argMat ./ normMat;
 end
