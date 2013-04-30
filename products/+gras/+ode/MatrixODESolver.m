db205f4f24d6707c5509547d668273ab5f29064c
pgagarinov <pgagarinov@gmail.com>
2013-04-30 18:08:45 +0000

    issue #72 BugFix: bug with (t0 == t1) was fixed.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_72_yadmiralsky@1506 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+gras/+ode/MatrixODESolver.m
+++ products/+gras/+ode/MatrixODESolver.m
@@ -12,20 +12,32 @@ classdef MatrixODESolver
         end
         function [timeVec,xResArray]=solve(self,fDerivFunc,timeVec,initVal)
             import modgen.common.throwerror;
-            sizeVec=self.sizeVec;
-            reshapeSizeVec=[sizeVec,ones(1,max(0,2-length(sizeVec)))];
-            if ~isequal(reshapeSizeVec,size(initVal))
-                throwerror('wrongInput',['initial value should be ',...
-                    'consistent with sizeVec specified in ',...
+            sizeVec = self.sizeVec;
+            reshapeSizeVec=[sizeVec, ones(1, max(0, 2 - length(sizeVec)))];
+            if ~isequal(reshapeSizeVec, size(initVal))
+                throwerror('wrongInput', ['initial value should be ', ...
+                    'consistent with sizeVec specified in ', ...
                     'the constructor']);
             end
-            fMatrixDerivFunc=@(t,y)(reshape(fDerivFunc(t,...
-                reshape(y,reshapeSizeVec)),[],1));
-            [timeVec,xResMat]=self.fSolveFunc(fMatrixDerivFunc,timeVec,...
-                initVal(:),odeset(self.odePropList{:}));
-            timeVec=timeVec.';
-            nTimePoints=length(timeVec);
-            xResArray=reshape(xResMat.',[sizeVec nTimePoints]);
+            timeSizeVec = size(timeVec);
+            minTimeVecSize = min(timeSizeVec);
+            [~, maxTimeVecInd] = max(timeSizeVec);
+            if (minTimeVecSize ~= 1) || (maxTimeVecInd > 2)
+                throwerror('wrongInput', 'time span is not a vector');
+            end
+            if timeVec(1) ~= timeVec(end)
+                fMatrixDerivFunc=@(t, y)(reshape(fDerivFunc(t, ...
+                    reshape(y, reshapeSizeVec)), [], 1));
+                [timeVec, xResMat] = ...
+                    self.fSolveFunc(fMatrixDerivFunc, timeVec, ...
+                    initVal(:), odeset(self.odePropList{:}));
+                timeVec = timeVec.';
+                nTimePoints = length(timeVec);
+                xResArray = reshape(xResMat.',[sizeVec nTimePoints]);
+            else
+                timeVec = timeVec(1);
+                xResArray = reshape(initVal, sizeVec);
+            end
         end
     end
 end
