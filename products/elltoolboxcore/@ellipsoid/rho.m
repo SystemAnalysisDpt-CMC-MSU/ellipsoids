7cd05ce6dc504ef752ee2c77a466bdd23f0eafce
pgagarinov <pgagarinov@gmail.com>
2013-05-06 18:12:01 +0000

    issue 38: update rho.m to new version
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_38_ilyubich@1566 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/elltoolboxcore/@ellipsoid/rho.m
+++ products/elltoolboxcore/@ellipsoid/rho.m
@@ -1,24 +1,24 @@
-function [supArr, bpMat] = rho(ellArr, dirsMat)
+function [supArr, bpArr] = rho(ellArr, dirsArr)
 %
 % RHO - computes the values of the support function for given ellipsoid
-%	and given direction.
+%       and given direction.
 %
-%	supArr = RHO(ellArr, dirsMat)  Computes the support function of the 
-%       ellipsoid ellArr in directions specified by the columns of matrix 
-%       dirsMat. Or, if ellArr is array of ellipsoids, dirsMat is expected 
+%       supArr = RHO(ellArr, dirsMat)  Computes the support function of the
+%       ellipsoid ellArr in directions specified by the columns of matrix
+%       dirsMat. Or, if ellArr is array of ellipsoids, dirsMat is expected
 %       to be a single vector.
 %
-%	[supArr, bpMat] = RHO(ellArr, dirstMat)  Computes the support function 
-%       of the ellipsoid ellArr in directions specified by the columns of 
-%       matrix dirsMat, and boundary points bpMat of this ellipsoid that 
-%       correspond to directions in dirsMat. Or, if ellArr is array of 
-%       ellipsoids, and dirsMat - single vector, then support functions and 
-%       corresponding boundary points are computed for all the given 
+%       [supArr, bpArr] = RHO(ellArr, dirstMat)  Computes the support function
+%       of the ellipsoid ellArr in directions specified by the columns of
+%       matrix dirsMat, and boundary points bpArr of this ellipsoid that
+%       correspond to directions in dirsMat. Or, if ellArr is array of
+%       ellipsoids, and dirsMat - single vector, then support functions and
+%       corresponding boundary points are computed for all the given
 %       ellipsoids in the array in the specified direction dirsMat.
 %
-%	The support function is defined as
+%       The support function is defined as
 %   (1)  rho(l | E) = sup { <l, x> : x belongs to E }.
-%	For ellipsoid E(q,Q), where q is its center and Q - shape matrix,
+%       For ellipsoid E(q,Q), where q is its center and Q - shape matrix,
 %   it is simplified to
 %   (2)  rho(l | E) = <q, l> + sqrt(<l, Ql>)
 %   Vector x, at which the maximum at (1) is achieved is defined by
@@ -32,7 +32,7 @@ function [supArr, bpMat] = rho(ellArr, dirsMat)
 %           double[nDim,nDirs]/[nDim,1] - array or matrix of directions.
 %
 % Output:
-%	supArr: double [nDims1,nDims2,...,nDimsN]/[1,nDirs] - support function
+%       supArr: double [nDims1,nDims2,...,nDimsN]/[1,nDirs] - support function
 %       of the ellArr in directions specified by the columns of matrix
 %       dirsMat. Or, if ellArr is array of ellipsoids, support function of
 %       each ellipsoid in ellArr specified by dirsMat direction.
@@ -50,46 +50,95 @@ function [supArr, bpMat] = rho(ellArr, dirsMat)
 %       31.8102    3.5394
 %
 % $Author: Alex Kurzhanskiy <akurzhan@eecs.berkeley.edu>
-% $Copyright:  The Regesnts of the University of California 2004-2008 $
+% $Copyright:  The Regesnts of the University of California 
+%              2004-2008 $
 %
-% $Author: Guliev Rustam <glvrst@gmail.com> $   $Date: Dec-2012$
-% $Copyright: Moscow State University,
+% $Author: Guliev Rustam <glvrst@gmail.com> $   
+% $Date: Dec-2012$
+% $Copyright: Lomonosov Moscow State University,
+%            Faculty of Computational Mathematics and Computer Science,
+%            System Analysis Department 2012 $
+%
+% $Author: Vitaly Baranov <vetbar42@gmail.com> $   $Date: 27-04-2013$
+% $Copyright: Lomonosov Moscow State University,
 %             Faculty of Computational Mathematics and Cybernetics,
-%             Science, System Analysis Department 2012 $
+%             Science, System Analysis Department 2013 $
 %
-
+%
 import modgen.common.checkmultvar;
-
+%
 ellipsoid.checkIsMe(ellArr,'first');
-modgen.common.checkvar(dirsMat, @(x)isa(x,'double'),...
+modgen.common.checkvar(dirsArr, @(x)isa(x,'double'),...
     'errorMessage','second argument must be matrix of direction vectors.');
-checkmultvar('isscalar(x1)||(size(x2,2)==1)',...
-    2,ellArr, dirsMat, 'errorMessage',...
-    'arguments must be single ellipsoid or single direction vector.');
-
-[nDim, ~] = size(dirsMat);
+%
+dirSizeVec = size(dirsArr);
+ellSizeVec=size(ellArr);
 isOneEll = isscalar(ellArr);
-
+isOneDir = dirSizeVec(2)==1 && length(dirSizeVec)==2;
+%
+nEll=prod(ellSizeVec);
+nDim=dirSizeVec(1);
+nDirs=prod(dirSizeVec(2:end));
+%
+if ~(isOneEll || isOneDir || ...
+        nEll==nDirs&&(ellSizeVec(1)==1 || ellSizeVec(2)==1)&&...
+        length(dirSizeVec)==2 ||...
+        all(ellSizeVec==dirSizeVec(2:end)))
+    modgen.common.throwerror('wrongInput:wrongSizes',...
+        strcat('arguments must be single ellipsoid or single direction ',...
+        'vector or arrays of almost the same sizes'));
+end
+%
 nDimsArr = dimension(ellArr);
 checkmultvar('all(x2==x1)',2,nDim,nDimsArr(:), 'errorMessage',...
     'dimensions mismatch.');
-
-if ~isOneEll % multiple ellipsoids, one direction
-    [resCArr xCArr] =arrayfun(@(x) fSingleRhoForOneDir(x),ellArr,...
+%
+if isOneEll % one ellipsoid, multiple directions
+    cenVec = ellArr.centerVec;
+    ellMat = ellArr.shapeMat;
+    [~, absTol] = getAbsTol(ellArr);
+    dirsMat=reshape(dirsArr,nDim,nDirs);
+    %
+    [supArr bpArr] =fSingleRhoForOneEll(dirsMat);
+    if length(dirSizeVec)>2
+        supArr=reshape(supArr,dirSizeVec(2:end));
+        bpArr=reshape(bpArr,dirSizeVec);
+    end
+elseif isOneDir % multiple ellipsoids, one direction
+    fComposite=@(ellObj)fRhoForDir(ellObj,dirsArr);
+    [resCArr xCArr]=arrayfun(fComposite,ellArr,...
         'UniformOutput',false);
     supArr = cell2mat(resCArr);
-    bpMat= horzcat(xCArr{:});
-else % one ellipsoid, multiple directions
-    qVec = ellArr.centerVec;
-    shMat = ellArr.shapeMat;
-    [~, absTol] = getAbsTol(ellArr);
-    [supArr,bpMat] = ellipsoid.rhomat(shMat,qVec,absTol,dirsMat);
+    bpArr = horzcat(xCArr{:});
+    if length(ellSizeVec)>2
+        reshVec=[nDim,ellSizeVec];
+        bpArr = reshape(bpArr,reshVec);
+    end
+else % multiple ellipsoids, multiple directions
+    augxCArr=num2cell(dirsArr,1);
+    dirCArr=reshape(augxCArr(1,:),ellSizeVec);
+    %
+    fComposite=@(ellObj,lVec)fRhoForDir(ellObj,lVec{1});
+    [resCArr xCArr]=arrayfun(fComposite,ellArr,dirCArr,...
+        'UniformOutput',false);
+    supArr = cell2mat(resCArr);
+    bpArr= horzcat(xCArr{:});
+    bpArr=reshape(bpArr,dirSizeVec);
 end
-
-    function [supFun xVec] = fSingleRhoForOneDir(singEll)
-        cVec  = singEll.centerVec;
-        shpMat  = singEll.shapeMat;
-        [~, singAbsTol] = getAbsTol(singEll);
-        [supFun,xVec] = ellipsoid.rhomat(shpMat,cVec,singAbsTol,dirsMat);
+%
+    function [supFun xVec] = fRhoForDir(ellObj,dirVec)
+        import gras.gen.sqrtpos;
+        [cenVec ellMat]=double(ellObj);
+        absTol=ellObj.getAbsTol();
+        sq  = sqrtpos(dirVec'*ellMat*dirVec,absTol);
+        supFun = cenVec'*dirVec + sq;
+        xVec = ((ellMat*dirVec)/sq) + cenVec;
+    end
+    function [supFun xVec] = fSingleRhoForOneEll(dirMat)
+        import gras.gen.sqrtpos;
+        tempMat  = sqrtpos(sum(dirsMat'*ellMat.*dirsMat',2), absTol);
+        supFun = cenVec'*dirMat + tempMat';
+        xVec =((ellMat*dirsMat)./repmat(tempMat',size(ellMat,1),1))+...
+            repmat(cenVec,1,size(dirsMat,2));
     end
 end
