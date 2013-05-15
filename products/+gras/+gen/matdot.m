6597b3127b2ac74bf176f917f09350a0f08f642b
pgagarinov <pgagarinov@gmail.com>
2013-05-15 12:29:49 +0000

    issue #72 Enhancement: added 'matdot' function.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_72_yadmiralsky@1675 fbfa2205-8925-0410-8e01-63a555b7045b

---
+++ products/+gras/+gen/matdot.m
@@ -0,0 +1,27 @@
+function resArr = matdot(inpArr1, inpArr2)
+% MATDOT calculates the dot production of two square matrices, which have
+% the same size.
+%
+% Input:
+%     regular:
+%         inpArr1: double[nMatDim,nMatDim,nElemsDim1...,nElemsDimK]
+%         inpArr2: double[nMatDim,nMatDim,nElemsDim1...,nElemsDimK]
+%
+% Output:
+%   resArr: double[nElemsDim1,...,nElemsDimk] - dot production values
+    import modgen.common.throwerror;
+    %
+    sizeVec = size(inpArr1);
+    if any(sizeVec ~= size(inpArr2))
+        throwerror('wrongInput:differentSize', ...
+            'input matrices have the different size');
+    end
+    if sizeVec(1) ~= sizeVec(2)
+        throwerror('wrongInput:notSquare', ...
+            'input matrices aren''t square matrices or square matrix arrays');
+    end
+    nMatDim = sizeVec(1);
+    resArr = sum(dot(inpArr1, inpArr2, 2), 1);
+    resArr = resArr / nMatDim;
+end
+
