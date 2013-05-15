926d5fc5e1f8999f61020070597db0487064436c
pgagarinov <pgagarinov@gmail.com>
2013-05-15 17:18:42 +0000

    Issue 118 Enhancement: modified run_reachcont_proj_adv_tests and ContinuousReachProjAdvTestCase to decrease runtime of this test changed dimensionality vector for tests in ContinuousReachRefineTestCase
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_118_dkhristoforov@1685 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+reach/+test/run_reachcont_proj_adv_tests.m
+++ products/+elltool/+reach/+test/run_reachcont_proj_adv_tests.m
@@ -1,4 +1,25 @@
 function results = run_reachcont_proj_adv_tests(inpConfAdvTestList, inpModeAdvTestList)
+% 'fix' - a mode with fixed projection matrix
+% 'rand' - a mode with random projection matrix
+%
+% elltool.reach.test.run_reachcont_proj_adv_tests() - run test for all
+%       configurations in 'fix' mode.
+%
+% elltool.reach.test.run_reachcont_proj_adv_tests({'conf1'}) - run test for
+%       'conf1' configuration in 'fix' mode.
+%
+% If you want to run additionaly 'rand' mode you can
+% do one of the folowing:
+%
+% elltool.reach.test.run_reachcont_proj_adv_tests('*',{'rand'}) - run test
+%       for all configurations in 'rand' mode.
+%
+% elltool.reach.test.run_reachcont_proj_adv_tests('*','*') - run test
+%       for all configurations in both 'rand' mode and 'fix' mode.
+%
+% elltool.reach.test.run_reachcont_proj_adv_tests({'conf1'},'*') - run test
+%       in 'conf1' configuration in both 'rand' mode and 'fix' mode.
+%
 import modgen.common.throwerror;
 import modgen.cell.cellstr2expression;
 %
@@ -8,47 +29,47 @@ loader = mlunitext.test_loader;
 crm = gras.ellapx.uncertcalc.test.regr.conf.ConfRepoMgr();
 crmSys = gras.ellapx.uncertcalc.test.regr.conf.sysdef.ConfRepoMgr();
 %
-FULL_CONF_LIST = {'ltisys', 'test2dbad'};
-FULL_MODE_LIST = {'fix','rand'};
+DEFAULT_CONF_LIST = {'ltisys', 'test2dbad'};
+DEFAULT_MODE_LIST = {'fix'};
+ALLOWED_MODE_LIST = {'fix','rand'};
 suiteList = {};
 %
 if nargin < 1
-    modeList = FULL_MODE_LIST;
-    confList = FULL_CONF_LIST;
+    modeList = DEFAULT_MODE_LIST;
+    confList = DEFAULT_CONF_LIST;
 else
     nInpConf = numel(inpConfAdvTestList);
-    nInpMode = numel(inpModeAdvTestList);
-    if (nargin < 2)||(nInpMode==1 && strcmp(inpModeAdvTestList,'*'))
-        modeList = FULL_MODE_LIST;
-    elseif sum(ismember(inpModeAdvTestList,FULL_MODE_LIST)) == numel(inpModeAdvTestList)
+    nInpMode = numel(inpModeAdvTestList);   
+    if nargin < 2
+        modeList = DEFAULT_MODE_LIST;
+    elseif (nInpMode==1 && strcmp(inpModeAdvTestList,'*'))
+        modeList = ALLOWED_MODE_LIST;
+    elseif sum(ismember(inpModeAdvTestList,DEFAULT_MODE_LIST)) == numel(inpModeAdvTestList)
         modeList = inpModeAdvTestList;
     else
         throwerror('wrongInput:unknownMode',...
             'Unexpected input mode: %s. Allowed modes: %s.',...
-             inpModeAdvTestList{~ismember(inpModeAdvTestList,FULL_MODE_LIST)},...
-             cellstr2expression(FULL_MODE_LIST));
+             inpModeAdvTestList{~ismember(inpModeAdvTestList,ALLOWED_MODE_LIST)},...
+             cellstr2expression(ALLOWED_MODE_LIST));
     end    
     %
     if (nInpConf==1 && strcmp(inpConfAdvTestList,'*'))
-        confList = FULL_CONF_LIST;
-    elseif sum(ismember(inpConfAdvTestList,FULL_CONF_LIST)) == numel(inpConfAdvTestList)
+        confList = DEFAULT_CONF_LIST;
+    elseif sum(ismember(inpConfAdvTestList,DEFAULT_CONF_LIST)) == numel(inpConfAdvTestList)
         confList = inpConfAdvTestList;
     else
         throwerror('wrongInput:unknownConf',...
             'Unexpected input configuration: %s. Allowed configurations: %s.',...
-             inpConfAdvTestList{~ismember(inpConfAdvTestList,FULL_CONF_LIST)},...
-             cellstr2expression(FULL_CONF_LIST));
+             inpConfAdvTestList{~ismember(inpConfAdvTestList,DEFAULT_CONF_LIST)},...
+             cellstr2expression(DEFAULT_CONF_LIST));
     end
 end    
 %
 nConf = numel(confList);
-nMode = numel(modeList);
 for iConf = 1:nConf
-    for iMode = 1:nMode
         suiteList{end + 1} = loader.load_tests_from_test_case(...
             'elltool.reach.test.mlunit.ContinuousReachProjAdvTestCase',...
-            confList{iConf}, crm, crmSys, modeList{iMode});
-    end
+            confList{iConf}, crm, crmSys, modeList);
 end    
 %
 testLists=cellfun(@(x)x.tests,suiteList,'UniformOutput',false);
