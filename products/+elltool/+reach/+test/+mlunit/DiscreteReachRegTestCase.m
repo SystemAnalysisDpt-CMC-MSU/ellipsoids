1910e19de0d3f0979bb217957c6c989dc7593c3d
pgagarinov <pgagarinov@gmail.com>
2013-06-12 15:09:02 +0000

    issue #72 BugFix: Changed regularization test in DiscreteReachRegTestCase.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_72_yadmiralsky@2010 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+reach/+test/+mlunit/DiscreteReachRegTestCase.m
+++ products/+elltool/+reach/+test/+mlunit/DiscreteReachRegTestCase.m
@@ -87,10 +87,10 @@ classdef DiscreteReachRegTestCase < mlunitext.test_case
             
             % 1
             
-            timeVec = [0 24];
-            ControlBoundsTest = ellipsoid(eye(2));
+            timeVec = [0 23];
+            ControlBoundsTest = 100 * ellipsoid(eye(2));
             linSys = elltool.linsys.LinSysDiscrete(...
-                atDefCMat, btDefCMat, ControlBoundsTest);
+                atDefCMat, btDefCMat, ControlBounds);
             self.runAndCheckError(...
                 ['elltool.reach.ReachDiscrete(linSys, x0Ell,',...
                 'l0Mat, timeVec, ''isRegEnabled'', true, ',...
