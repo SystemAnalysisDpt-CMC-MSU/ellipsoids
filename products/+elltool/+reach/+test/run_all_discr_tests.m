2cb7be2729bb50a94637d1ecdf2f932111f8aeb6
pgagarinov <pgagarinov@gmail.com>
2013-06-16 18:14:03 +0000

    Issue 90 BugFix: fixed small misprint
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_90_dstepensky@2043 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+reach/+test/run_all_discr_tests.m
+++ products/+elltool/+reach/+test/run_all_discr_tests.m
@@ -1,5 +1,5 @@
 function results = run_all_discr_tests(varargin)
-resList{1} = elltoo.reach.test.run_most_discr_tests(varargin{:});
+resList{1} = elltool.reach.test.run_most_discr_tests(varargin{:});
 testCaseNameStr = 'elltool.reach.test.mlunit.DiscreteReachProjAdvTestCase';
 resList{2} = elltool.reach.test.run_reach_proj_adv_tests(testCaseNameStr);
 results = [resList{:}];
