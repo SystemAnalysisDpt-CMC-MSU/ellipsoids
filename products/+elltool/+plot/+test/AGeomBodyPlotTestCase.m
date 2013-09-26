a282ffe0fa27ec934a8b81790e71a2e5b0223b25
sdrozhzhin <SeregaDrozh@gmail.com>
2013-09-26 13:27:51 +0000

    Issue 77 Enhancement: modify colorcode2rgb.m. Written Positive test.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_77_sdrozhzhin@2282 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+plot/+test/AGeomBodyPlotTestCase.m
+++ products/+elltool/+plot/+test/AGeomBodyPlotTestCase.m
@@ -26,6 +26,17 @@ classdef AGeomBodyPlotTestCase < mlunitext.test_case
                 'wrongInput');
         end
         
+        function self = testPositiveRGB(self)
+            mlunitext.assert_equals(elltool.plot.colorcode2rgb('r'), [1 0 0]);
+            mlunitext.assert_equals(elltool.plot.colorcode2rgb('g'), [0 1 0]);
+            mlunitext.assert_equals(elltool.plot.colorcode2rgb('b'), [0 0 1]);
+            mlunitext.assert_equals(elltool.plot.colorcode2rgb('y'), [1 1 0]);
+            mlunitext.assert_equals(elltool.plot.colorcode2rgb('c'), [0 1 1]);
+            mlunitext.assert_equals(elltool.plot.colorcode2rgb('m'), [1 0 1]);
+            mlunitext.assert_equals(elltool.plot.colorcode2rgb('w'), [1 1 1]);
+            mlunitext.assert_equals(elltool.plot.colorcode2rgb('k'), [0 0 0]);
+        end
+        
         function self = testWrongInput(self)
             
             testFirEll = self.getInstance(eye(2));
--- products/+elltool/+plot/colorcode2rgb.m
+++ products/+elltool/+plot/colorcode2rgb.m
@@ -13,18 +13,21 @@ function rgbVec = colorcode2rgb(colorCode)
 %            Faculty of Computational Mathematics and Cybernetics,
 %            System Analysis Department 2013 $
 
+AVAILABLE_COLORS = 'kbgcrmyw';
+
 if ~(ischar(colorCode))
     modgen.common.throwerror('wrongInput',...
         'input must be char');
 elseif (numel(colorCode) >= 2 || numel(colorCode)==0)
     modgen.common.throwerror('wrongInput',...
         'input must be char[1,1]');
-elseif ( isempty(strfind('kbgcrmyw', colorCode)) )
-    modgen.common.throwerror('wrongInput',...
-        'input must be one of the following symbols : r, g, b, y, c , m, w or k');
 else
-    rgbVec = rem(floor((strfind('kbgcrmyw', colorCode) - 1) * [0.25 0.5 1]),2);
+    COLORCODE = strfind(AVAILABLE_COLORS, colorCode);
+    if ( isempty(COLORCODE) )
+        modgen.common.throwerror('wrongInput',...
+            'input must be one of the following symbols : r, g, b, y, c , m, w or k');
+    end
 end
+rgbVec = rem(floor((COLORCODE - 1) * [0.25 0.5 1]),2);
 end
-
 
