b14176f1d60696e97b3db746c674e693882538fd
sdrozhzhin <SeregaDrozh@gmail.com>
2013-09-26 16:21:46 +0000

    Issue 77 Enhancement: modify colorcode2rgb.m.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_77_sdrozhzhin@2289 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+plot/colorcode2rgb.m
+++ products/+elltool/+plot/colorcode2rgb.m
@@ -13,21 +13,21 @@ function rgbVec = colorcode2rgb(colorCode)
 %            Faculty of Computational Mathematics and Cybernetics,
 %            System Analysis Department 2013 $
 
-AVAILABLE_COLORS = 'kbgcrmyw';
+AVAILABLE_COLORS_VEC = 'kbgcrmyw';
 
 if ~(ischar(colorCode))
     modgen.common.throwerror('wrongInput',...
         'input must be char');
-elseif (numel(colorCode) >= 2 || numel(colorCode)==0)
+elseif (~iscalar(colorCode))
     modgen.common.throwerror('wrongInput',...
         'input must be char[1,1]');
 else
-    colorCodeInd = strfind(AVAILABLE_COLORS, colorCode);
-    if ( isempty(colorCodeInd) )
+    indColorCode = strfind(AVAILABLE_COLORS_VEC, colorCode);
+    if ( isempty(indColorCode) )
         modgen.common.throwerror('wrongInput',...
             'input must be one of the following symbols : r, g, b, y, c , m, w or k');
     end
 end
-rgbVec = rem(floor((colorCodeInd - 1) * [0.25 0.5 1]),2);
+rgbVec = rem(floor((indColorCode - 1) * [0.25 0.5 1]),2);
 end
 
