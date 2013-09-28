b4be3ab39f68f3270e53faeadf7d37202a4ad829
eshcherbakova <shcherbakova415@gmail.com>
2013-09-28 20:24:59 +0000

    Issue 130 Enhancement: For picture named minksum.eps in doc/pic I wrote a separate function in elltool.doc.picgen named minksum_gen.m which accepts a target file name for a picture and then generates this picture.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_130_eshcherbakova@2306 fbfa2205-8925-0410-8e01-63a555b7045b

---
+++ products/+elltool/+doc/+picgen/minksum_gen.m
@@ -0,0 +1,29 @@
+function minksum_gen (FileName)
+%     MINKSUM_GEN - creates picture "FileName.eps" 
+% Input:
+%     regular:
+%         FileName: char [1, ] - target file name for a picture.    
+% $Author: <Elena Shcherbakova>  <shcherbakova415@gmail.com> $    $Date: <27 September 2013> $
+% $Copyright: Moscow State University,
+%            Faculty of Computational Mathematics and Cybernetics,
+%            System Analysis Department 2013 $
+if ~(ischar(FileName))
+    modgen.common.throwerror('wrongInput',...
+        'Input must be char');
+elseif ~(size(FileName, 1))
+    modgen.common.throwerror('wrongInput',...
+        'Input must be char[1, ]');
+else
+    elltool.doc.snip.s_chapter05_section01_snippet02;
+    elltool.doc.snip.s_chapter05_section01_snippet15;
+    h = get(gca,'Children');
+    centrX = get(h(1), 'XData');
+    centrY = get(h(1), 'YData');
+    ellX = get(h(2), 'XData');
+    ellY = get(h(2), 'YData');
+    fill (ellX, ellY, [0, 1, 0.5]);
+    hold on
+    plot (centrX, centrY, 'Marker', '*', 'MarkerSize', 8, 'Color', 'b');
+    saveas (gcf,  strcat (FileName, '.eps'));
+end
+end
