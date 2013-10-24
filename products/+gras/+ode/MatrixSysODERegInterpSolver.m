19380decf8c773c6d5df10f55fd12163ff516f29
vadimdanilov93 <vadimdanilov93@gmail.com>
2013-10-24 15:33:24 +0000

    Issue 126 Enhancement: help headers was added in MatrixSysODERegInterpSolver and MatrixSysReshapeOde45RegInterp classes.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_126_vadimdanilov@2468 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+gras/+ode/MatrixSysODERegInterpSolver.m
+++ products/+gras/+ode/MatrixSysODERegInterpSolver.m
@@ -1,12 +1,61 @@
 classdef MatrixSysODERegInterpSolver < gras.ode.MatrixSysODESolver
     methods
         function self=MatrixSysODERegInterpSolver(varargin)
+            % MATRIXSYSODEREGINTERPSOLVER - MatrixSysODERegInterpSolver
+            %   class constructor. This class inherits from 
+            %   gras.ode.MatrixSysODESolver. Differences of class 
+            %   gras.ode.MatrixSysODESolver and 
+            %   gras.ode.MatrixSysODERegInterpSolver lie in the fact that
+            %   the method gras.ode.MatrixSysODERegInterpSolver.solve()
+            %   further returns an object of class
+            %   gras.ode.MatrixSysReshapeOde45RegInterp.
+            %
+            % $Author: Vadim Danilov <vadimdanilov93@gmail.com> $  
+            % $Date: 24-10-2013$
+            % $Copyright: Moscow State University,
+            %  Faculty of Computational Mathematics and Cybernetics,
+            %  Science, System Analysis Department 2013 $
             self = self@gras.ode.MatrixSysODESolver(varargin{:});
         end
         function [timeVec,varargout]=solve(self,fDerivFuncList,timeVec,...
                 varargin)
+            % SOLVE - solver of the system of matrix equations
+            % Input:
+            %   regular:
+            %       self: gras.ode.MatrixSysODERegInterpSolver[1,1] -
+            %           all the data nessecary to solve the system of
+            %           matrix equations  is stored in this object;
+            %       fDerivFuncList: cell[1,nEquations] of function handle -
+            %           list of derivatives functions;
+            %       timeVec: double[1,nPoints] - time range, same meaning 
+            %           as in ode45;
+            %       varargin: cell[1,nKnots] - list of different parameters
+            %           solver (see ode45 op ode45reg);
+            % Output:
+            %   timeVec: double[nPoints,1] - time grid, same meaning
+            %       as in ode45
+            %   varargout: cell[1,nargout-1]
+            %       varargout(1:(end-1)): cell[1,nargout-2] of double
+            %           array - cell vector, which contains
+            %           nEquations*nFuncs arrays of dobule (nEquations
+            %           for each function), each of which is a solution of
+            %           the corresponding equation for the corresponding
+            %           function.
+            %       varargout(end):
+            %           gras.ode.MatrixSysReshapeOde45RegInterp[1,1] - 
+            %           all the data nessecary for calculation on an
+            %           arbitrary time grid is stored in this object,
+            %           including the dimensionality of the system and the 
+            %           number of functions; in fact it is shell of 
+            %           gras.ode.VecOde45RegInterp for system of matrix
+            %           equations. 
+            % $Author: Vadim Danilov <vadimdanilov93@gmail.com> $  
+            % $Date: 24-10-2013$
+            % $Copyright: Moscow State University,
+            %  Faculty of Computational Mathematics and Cybernetics,
+            %  Science, System Analysis Department 2013 $
+            
             nFuncs = length(fDerivFuncList);
-            % if nFuncs = 1, then objVecOde45RegInterp not return
             nOuts = length(self.sizeEqList)*nFuncs;
             resList = cell(1,nOuts);
             resAdvList = cell(1,nFuncs-1);
--- products/+gras/+ode/MatrixSysReshapeOde45RegInterp.m
+++ products/+gras/+ode/MatrixSysReshapeOde45RegInterp.m
@@ -1,23 +1,87 @@
 classdef MatrixSysReshapeOde45RegInterp
     properties(Access=private)
-        VecOde45RegInterpObj
+        objVecOde45RegInterp
         sizeEqList
         nFuncs
     end
     methods
         function interpObj = MatrixSysReshapeOde45RegInterp(...
-                VecOde45RegInterpObj,sizeEqList,nFuncs)
+                objVecOde45RegInterp,sizeEqList,nFuncs)
+            % MATRIXSYSRESHAPEODE45REGINTERP - 
+            %       MatrixSysReshapeOde45RegInterp class constructor
+            % Input:
+            %   regular:
+            %       objVecOde45RegInterp: gras.ode.VecOde45RegInterp[1,1] -
+            %           all the data nessecary for calculation on an
+            %           arbitrary time grid is stored in this object;
+            %       sizeEqList: cell[1,nEquations] of double array - sizes 
+            %           of all equations in system of matrix equations
+            %           stored in this list;
+            %       nFuncs: double[1,1] - the number of functions to
+            %           compute the result; if nFuncs equal 1, then result 
+            %           compute only for derivative function; if nFuncs 
+            %           equal 2, then result compute for derivative 
+            %           function and regularization function;
+            % Output:
+            %   interpObj: gras.ode.MatrixSysReshapeOde45RegInterp[1,1] - 
+            %       all the data nessecary for calculation on an
+            %       arbitrary time grid is stored in this object,
+            %       including the dimensionality of the system and the 
+            %       number of functions; in fact it is shell of 
+            %       gras.ode.VecOde45RegInterp for system of matrix
+            %       equations.
+            % $Author: Vadim Danilov <vadimdanilov93@gmail.com> $  
+            % $Date: 24-10-2013$
+            % $Copyright: Moscow State University,
+            %  Faculty of Computational Mathematics and Cybernetics,
+            %  Science, System Analysis Department 2013 $
             
-                % don't forget parse parameters 
-            interpObj.VecOde45RegInterpObj = VecOde45RegInterpObj;
-            interpObj.sizeEqList = sizeEqList;
-            interpObj.nFuncs = nFuncs;
+          	import modgen.common.throwerror;
+            if(isequal(class(objVecOde45RegInterp),...
+                    'gras.ode.VecOde45RegInterp'))
+                interpObj.objVecOde45RegInterp = objVecOde45RegInterp;
+                interpObj.sizeEqList = sizeEqList;
+                interpObj.nFuncs = nFuncs;
+            else
+                throwerror('wrongInput',['the first argument ' ...
+                    'gras.ode.MatrixSysReshapeOde45RegInterp should ',...
+                    'be object of class gras.ode.VecOde45RegInterp']);
+            end;
         end
         
         function [timeVec,varargout] = evaluate(self,timeVec)
+            % EVALUATE - this method duplicates the function's 
+            %   gras.ode.MatrixSysODESolver.solve() work on an arbitrary
+            %   time grid timeVec
+            % Input:
+            %   regular:
+            %       self: gras.ode.MatrixSysReshapeOde45RegInterp[1,1] - 
+            %           all the data nessecary for calculation on an
+            %           arbitrary time grid is stored in this object,
+            %           including the dimensionality of the system and the 
+            %           number of functions; in fact it is shell of 
+            %           gras.ode.VecOde45RegInterp for system of matrix
+            %           equations.
+            %       timeVec: double[1,nPoints] - time range, same meaning 
+            %           as in ode45
+            %   Output:
+            %       timeVec: double[nPoints,1] - time grid, same meaning
+            %           as in ode45
+            %       varargout: cell[1,nargout-1] of double array - cell
+            %           vector, which contains nEquations*nFuncs arrays of
+            %           dobule (nEquations for each function), each of 
+            %           which is a solution of the corresponding equation
+            %           for the corresponding function.
+            %
+            % $Author: Vadim Danilov  <vadimdanilov93@gmail.com>
+            % $	$Date: 24-10-2013$
+            % $Copyright: Moscow State University,
+            %            Faculty of Computational Mathematics and Computer
+            %            Science, System Analysis Department 2013 $
+            
             resList = cell(1,self.nFuncs);
             [timeVec,resList{:}] = ...
-                self.VecOde45RegInterpObj.evaluate(timeVec);
+                self.objVecOde45RegInterp.evaluate(timeVec);
             nTimePoints = length(timeVec);
             nEquations = length(self.sizeEqList);
             nElemVec=cellfun(@prod,self.sizeEqList);
