30f2ada77a65effe4ae1377747a0a62e9008ec84
asemenova <anastasia.semenova424@gmail.com>
2013-10-30 20:29:15 +0000

    issue 74 Enhancement: ContinuousSecReachTestCase, DiscreteSecReachTestCase and ATestDynGettersBaseTestCase were rewritten. Now ATestDynGettersBaseTestCase is abstract class.
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_74_asemenova@2502 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+reach/+test/+mlunit/ContinuousSecReachTestCase.m
+++ products/+elltool/+reach/+test/+mlunit/ContinuousSecReachTestCase.m
@@ -1,51 +1,14 @@
-%classdef ContinuousSecReachTestCase < elltool.reach.test.mlunit.TestDynGettersBaseTestCase
-classdef ContinuousSecReachTestCase < mlunitext.test_case
-    properties (Access=private)
-        testDataRootDir
-        linSys
-        reachObj
-        tVec
-        x0Ell
-        l0Mat
-        expDim
-        reachFactoryObj
-        
-    end
+classdef ContinuousSecReachTestCase < elltool.reach.test.mlunit.ATestDynGettersBaseTestCase
+    
     methods
-        function self = ContinuousSecReachTestCase(varargin)
-            %TestDynGettersBaseTestCase(varargin{:});
-            self = self@mlunitext.test_case(varargin{:});
-            [~, className] = modgen.common.getcallernameext(1);
-            shortClassName = mfilename('classname');
-            self.testDataRootDir = [fileparts(which(className)),...
-                filesep, 'TestData', filesep, shortClassName];
-            %bpbFunc=@(probDynObj,curTime) probDynObj.getBPBTransDynamics().evaluate(curTime);
-           %elltool.reach.test.mlunit.TestDynGettersBaseTestCase.baseTestDynGetters(bpbFunc);
-            %elltool.reach.test.mlunit.TestDynGettersBaseTestCase(varargin{:});
-            %baseTestDynGetters(bpbFunc);
-
-        end
-        %
-        function self = set_up_param(self, reachFactObj)
-            self.reachFactoryObj=reachFactObj;
-            self.reachObj = reachFactObj.createInstance();
-            self.linSys = reachFactObj.getLinSys();
-            self.expDim = reachFactObj.getDim();
-            self.tVec = reachFactObj.getTVec();
-            self.x0Ell = reachFactObj.getX0Ell();
-            self.l0Mat = reachFactObj.getL0Mat();
+        function self = ContinuousSecReachTestCase(varargin)            
+            self = self@elltool.reach.test.mlunit.ATestDynGettersBaseTestCase(varargin{:});            
+        end        
+    end
+    
+    methods(Access = protected)
+        function bpbMat = bpbFunc(self,pDynBPBMat,probDynObj,curTime)
+            bpbMat=pDynBPBMat;
         end
     end
-   methods
-       function self = testDynGetters(self)
-           
-           bpbFunc=@(pDynBPBMat,probDynObj,curTime) pDynBPBMat;
-           %isOk=elltool.reach.test.mlunit.TestDynGettersBaseTestCase.baseTestDynGetters(reachObj,bpbFunc);
-           %baseTestDynGetters(bpbFunc);
-           baseTestObj=elltool.reach.test.mlunit.TestDynGettersBaseTestCase(self.reachObj,self.reachFactoryObj,bpbFunc);
-           isOk=baseTestObj.baseTestDynGetters();
-           mlunitext.assert_equals(true, isOk);
-           
-       end;           
-   end
 end
