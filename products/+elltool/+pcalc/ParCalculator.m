4dd14edd60a1ca725478fa4ee3e469ba0e84778d
ezilonova <zilonova.e.m@gmail.com>
2013-12-05 06:52:38 +0000

    Issue 131 Enhancement: added help header for eval in ParCalculator.m
    git-svn-id: https://ellipsoids.googlecode.com/svn/branches/issue_131_ezilonova@2654 fbfa2205-8925-0410-8e01-63a555b7045b

--- products/+elltool/+pcalc/ParCalculator.m
+++ products/+elltool/+pcalc/ParCalculator.m
@@ -14,6 +14,28 @@ classdef ParCalculator
 %    
     methods (Static)
       function [varargout]=eval(f, varargin)
+            %  EVAL -  reads clusterSize from the current
+            %      configuration and launches modgen.pcalc.auxdfeval
+            %      with this clusterSize for  parent processes and with
+            %      clusterSize=1 for child processes.
+            % Input:
+            %   regular:
+            %     f: function_handle[1,1]- function that is going to be
+            %        calculated in a parallel manner.
+            %   optional:
+            %     varargin: arguments for f. 
+            %     In case f takes no arguments, nothing is needed to be
+            %     passed.
+            % Output:
+            %      varargout: whatever function f returns.
+            %
+            % $Author: Zilonova Ekaterina
+            % <zilonova.e.m@gmail.com> $
+            % $Date: 4-december-2013 $
+            % $Copyright: Moscow State University,
+            %             Faculty of Computational Mathematics
+            %             and Computer Science,
+            %             System Analysis Department 2012 $
           nMaxParProcess=elltool.conf.Properties.getConfRepoMgr.getParam('parallelCompProps.nMaxParProcess');
           varargout=cell(1,nargout); 
           [taskName,SProp]=modgen.pcalc.gettaskname();
@@ -22,6 +44,7 @@ classdef ParCalculator
               
           else
             [varargout{:}]=modgen.pcalc.auxdfeval(f, varargin{:},'ClusterSize',nMaxParProcess);
+            
          end
        end 
    end
