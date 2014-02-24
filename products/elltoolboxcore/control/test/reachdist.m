a57f8c2062fcae56e8d9718bb3f1ef2f1478fdd5
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-02-24 16:53:09 +0400

    issue 79: reachdist.m

--- products/elltoolboxcore/control/test/reachdist.m
+++ products/elltoolboxcore/control/test/reachdist.m
@@ -1,4 +1,4 @@
-  T  = 5;
+  T  = [0 5];
   w  = 1;
   A  = [0 1; -w 0];
   %A  = {'0' '1'; '-1.5+cos(2*t)' '0'};
@@ -9,15 +9,15 @@
   U.shape =  [9 0; 0 2];
   V  = ellipsoid([1 -1; -1 3]);
   V  = ell_unitball(1);
-  s  = linsys(A, B, U, C, V);
+  s  = elltool.linsys.LinSysContinuous(A, B, U, C, V);
   %s  = linsys(A, B, U);
   
   X0 = ellipsoid([10; 0], [1 0; 0 1]);
   L0 = [1 -1; 1 1; 0 1]';
   L0 = [0 1; 1 -1; 1 2]';
  % L0 = [1 2]';
-  o.save_all = 1;
-  rs = reach(s, X0, L0, T, o);
+%   o.save_all = 1;
+  rs = elltool.reach.ReachContinuous(s, X0, L0, T,'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-3);
 
-  plotByEa(rs); hold on;
-  plotByIa(rs);
+  rs.plotByEa(rs);
+  rs.plotByIa(rs);
