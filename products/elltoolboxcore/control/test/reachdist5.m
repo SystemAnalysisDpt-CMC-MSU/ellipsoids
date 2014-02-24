127b7f62b3e151fe45bcc14577c28533a2c45c74
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-02-24 16:55:25 +0400

    issue 79: reachdist5.m

--- products/elltoolboxcore/control/test/reachdist5.m
+++ products/elltoolboxcore/control/test/reachdist5.m
@@ -10,11 +10,11 @@
   C  = eye(5);
   U  = ellipsoid([9 0 0 0 0; 0 2 0 0 0; 0 0 4 0 0; 0 0 0 4 0; 0 0 0 0 9]);
   V  = ellipsoid([1 0 0 0 0; 0 2 0 0 0; 0 0 3 0 0; 0 0 0 2 0; 0 0 0 0 1]);
-  o.save_all = 1;
-  s  = linsys(A, B, U, C, V);
+%   o.save_all = 1;
+  s  = elltool.linsys.LinSysContinuous(A, B, U, C, V);
   
   X0 = ellipsoid([10; -1; 1; 0; -10], 36*eye(5));
   L0 = [0 1 -1 0 1; 0 1 1 0 0; 1 0 0 1 -1]';
   L0 = [0 1 -1 0 1; 0 1 1 0 0]';
-  o.save_all = 1;
-  rs = reach(s, X0, L0, [1 T],o);
+%   o.save_all = 1;
+  rs = elltool.reach.ReachContinuous(s, X0, L0, [1 T],'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-3);
