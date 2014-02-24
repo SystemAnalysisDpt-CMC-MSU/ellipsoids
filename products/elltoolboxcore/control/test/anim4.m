886dd6b33fc14462b5d9b8d06f6c386fc5655f08
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-02-24 10:56:29 +0400

    issue 79: anim4.m

--- products/elltoolboxcore/control/test/anim4.m
+++ products/elltoolboxcore/control/test/anim4.m
@@ -2,16 +2,15 @@ import elltool.conf.Properties;
 
 C =0.25;
  A = [0 1; 0 0]; B = [0; 1]; U = ellipsoid(1);
- s = linsys(A, B, U);
+ s = elltool.linsys.LinSysContinuous(A, B, U);
 
  X0 = Properties.getAbsTol()*ell_unitball(2);
 
  L0 = [-1 -1; 1 0; 0 1; 2 1; 3 1; 1 3; 1 2; -1 1; -2 1; -3 1; -1 3; -1 2]';
 
- T = 6;
- o.approximation = 0;
- rs = reach(s, X0, L0, T, o);
- EA = get_ea(cut(rs, T));
+ T = [0 6];
+ rs = elltool.reach.ReachContinuous(s, X0, L0, T, 'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-3);
+ EA = rs.cut(T(end)).get_ea();
 
     EA  = inv(EA');
     M   = size(EA, 2);
@@ -32,46 +31,46 @@ C =0.25;
       x = l/realsqrt(mval);
       yy = [yy x];
     end
-    yy = [T*ones(1, N); yy];
+    yy = [T(end)*ones(1, N); yy];
 
 
- [xx, tt] = get_goodcurves(rs);
- LL       = get_directions(rs);
+ [xx, tt] = rs.get_goodcurves();
+ LL       = rs.get_directions();
  xx = xx{1};
- xi = [T; C*xx(:, end)];
+ xi = [T(end); C*xx(:, end)];
 
 
 
 
 
 
- clear MM;
-
- for i = 1:199
-	 cla;
-   x0 = C*xx(:, i);
-   X0 = x0 + Properties.getAbsTol()*ell_unitball(2);
-   t0 = tt(i);
-   L0 = [];
-   for j = 1:M
-	   L = LL{j};
-	   L0 = [L0 L(:, i)];
-   end
-
-   rs = reach(s, X0, L0, [t0 T], o);
-   plotByEa(rs); hold on;
-   ell_plot(yy, 'r', 'LineWidth', 2);
-   ell_plot(xi, 'ko');
-   ell_plot([t0; x0], 'k*');
-   ell_plot([tt(i:end); C*xx(:, i:end)], 'k');
-
-   title(sprintf('Reach tube at time T = %d', t0));
-  axis([0 T -40 40 -6 6]);
-
-   hold off;
-
-   MM(i) = getframe(h);
- end
-
- movie2avi(MM, 'internal_point.avi', 'QUALITY', 100);
+%  clear MM;
+% 
+%  for i = 1:199
+% 	 cla;
+%    x0 = C*xx(:, i);
+%    X0 = x0 + Properties.getAbsTol()*ell_unitball(2);
+%    t0 = tt(i);
+%    L0 = [];
+%    for j = 1:M
+% 	   L = LL{j};
+% 	   L0 = [L0 L(:, i)];
+%    end
+% 
+%    rs = elltool.reach.ReachContinuous(s, X0, L0, [t0 T(end)], 'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-3);
+%    rs.plotByEa(); hold on;
+%    ell_plot(yy, 'r', 'LineWidth', 2);
+%    ell_plot(xi, 'ko');
+%    ell_plot([t0; x0], 'k*');
+%    ell_plot([tt(i:end); C*xx(:, i:end)], 'k');
+% 
+%    title(sprintf('Reach tube at time T = %d', t0));
+%   axis([0 T -40 40 -6 6]);
+% 
+%    hold off;
+% 
+%    MM(i) = getframe(h);
+%  end
+% 
+%  movie2avi(MM, 'internal_point.avi', 'QUALITY', 100);
    
