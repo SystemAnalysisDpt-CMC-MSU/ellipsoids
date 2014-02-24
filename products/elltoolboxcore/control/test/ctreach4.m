f2250c1a0501e8f6de639654a8d453dfbab6d778
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-02-24 16:44:36 +0400

    issue 79: ctreach4.m

--- products/elltoolboxcore/control/test/ctreach4.m
+++ products/elltoolboxcore/control/test/ctreach4.m
@@ -1,14 +1,14 @@
-  T = 5;
+  T = [0 5];
   A = [0 1 0 0; -1 0 1 0; 0 0 0 1; 0 0 -1 0];
   B = [0; 0; 0; 1];
 
   X0 = ell_unitball(4) + [1; 1; 0; -1];
   U  = ellipsoid(1);
 
-  s  = linsys(A, B, U);
+  s  = elltool.linsys.LinSysContinuous(A, B, U);
 
   L  = [1 1 0 1; 0 -1 1 0; -1 1 1 1; 0 0 -1 1]';
  % L  = [1 1 0 1; 0 -1 1 0; -1 1 1 1]';
   L  = eye(4);
-  o.save_all = 1;
-  rs = reach(s, X0, L, T, o);
+%   o.save_all = 1;
+  rs = elltool.reach.ReachContinuous(s, X0, L, T, 'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-3);
