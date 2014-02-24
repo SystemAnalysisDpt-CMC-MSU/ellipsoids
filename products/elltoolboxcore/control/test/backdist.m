99cd6ff5facb77cfcb59c9add8ae4a0cdd576576
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-02-24 11:04:35 +0400

    issue 79: backdist.m

--- products/elltoolboxcore/control/test/backdist.m
+++ products/elltoolboxcore/control/test/backdist.m
@@ -5,9 +5,9 @@
   C  = eye(2);
   U  = ellipsoid([9 0; 0 2]);
   V  = ellipsoid([1 0; 0 2]);
-  s  = linsys(A, B, U, C, V);
+  s  = elltool.linsys.LinSysContinuous(A, B, U, C, V);
   
   X0 = ellipsoid([10; 0], [25 0; 0 25]);
   L0 = [1 -1; 1 1; 0 1]';
 %  L0 = [1 1]';
-  rs = reach(s, X0, L0, [T 0]);
+  rs = elltool.reach.ReachContinuous(s, X0, L0, [T 0], 'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-3);
