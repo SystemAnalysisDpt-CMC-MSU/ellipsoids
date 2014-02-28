35d7acc9c383f4db11686d13b35bbe45f204e821
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-02-28 19:37:53 +0400

    issue 79: circ1.m

--- products/elltoolboxcore/control/test/circ1.m
+++ products/elltoolboxcore/control/test/circ1.m
@@ -1,4 +1,4 @@
-o.save_all = 1;
+
 R = 4; L = 0.5; C = 0.1; 
 A = [0 -1/C; 1/L -R/L];
 B = [1/C 0; 0 1/L];
@@ -8,18 +8,18 @@ I = ell_unitball(2);
 U.center = {'1000/(t^2)'; 'sin(2*t)'};
 U.shape = [4 -1; -1 1];
 
-X0 = 0.00001*ell_unitball(2);
-T  = 10;
+X0 = 0.0001*ell_unitball(2);
+T  = [0 10];
 T1 = 20;
 L0 = [0 1; 1 1; 1 0; 1 -1]';
 %L0 = [0 1; 1 0]';
 
-s1  = linsys(A, B, I);
-s2  = linsys(A1, B1, U);
-rs1 = reach(s1, X0, L0, T, o);
-rs2 = evolve(rs1, T1, s2);
+s1  = elltool.linsys.LinSysContinuous(A, B, I);
+s2  = elltool.linsys.LinSysContinuous(A1, B1, U);
+rs1 = elltool.reach.ReachContinuous(s1, X0, L0, T, 'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-4);
+rs2 = rs1.evolve(T1, s2);
 
-plotByEa(rs1); hold on;
-plotByIa(rs1); hold on;
-plotByEa(rs2, 'r'); hold on;
-plotByIa(rs2, 'y'); hold on;
+rs1.plotByEa(); hold on;
+rs1.plotByIa(); hold on;
+rs2.plotByEa('r'); hold on;
+rs2.plotByIa('y'); hold on;
