fe76ae31ea949b64029feeb90b72df8778b392df
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-03-12 11:58:28 +0400

    issue 79: dtback.m

--- products/+elltool/+demo/+test/+control/dtback.m
+++ products/+elltool/+demo/+test/+control/dtback.m
@@ -2,25 +2,23 @@ function dtback
 % Test for discrete-time lin. system backward reachability.
 
 
-  Ad  = [cos(1) -sin(1); sin(1) cos(1)];
-  B   = [0; 1];
-  P   = ellipsoid(0, 1);
-  M   = ellipsoid([-1; 2], [4 -1; -1 1]);
+  aMat  = [cos(1) -sin(1); sin(1) cos(1)];
+  bVec   = [0; 1];
+  pEllObj   = ellipsoid(0, 1);
+  mEllObj   = ellipsoid([-1; 2], [4 -1; -1 1]);
   N = 20;
-%   phi = 0:0.1:2*pi;
   L0  = [1 1 0 1; 1 -1 1 0];
-%   L0  = [cos(phi); sin(phi)];
 
-  ds  = elltool.linsys.LinSysDiscrete(Ad, B, P, [], [], [], [], 'd');
-  rs  = elltool.reach.ReachDiscrete(ds, M, L0, [N 0]);
-  E   = rs.get_ea();
-  I   = rs.get_ia();
+  dSys  = elltool.linsys.LinSysDiscrete(aMat, bVec, pEllObj, [], [], [], [], 'd');
+  rsObj  = elltool.reach.ReachDiscrete(dSys, mEllObj, L0, [N 0]);
+  extApprox   = rsObj.get_ea();
+  intApprox   = rsObj.get_ia();
   
-  EA  = (Ad^(-N)) * M;
+  EA  = (aMat^(-N)) * mEllObj;
   for i = 1:N
-    F  = Ad^(N-i);
+    F  = aMat^(N-i);
     Fi = inv(F);
-    EA = [EA Fi*inv(Ad)*B*P];
+    EA = [EA Fi*inv(aMat)*bVec*pEllObj];
   end
 
   
