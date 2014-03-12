5a44e56a84af00780518f6126b29e7775d6f1b8f
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-03-12 11:52:13 +0400

    issue 79: ctreach.m

--- products/+elltool/+demo/+test/+control/ctreach.m
+++ products/+elltool/+demo/+test/+control/ctreach.m
@@ -1,7 +1,6 @@
 function ctreach
 % Continuous-time system reachability test.
 
-clear P1;
   firstAMat        = [0 1; 0 0];
   thirdAMat  = [0 1; -2 0];
   secondACMat = {'0' '1-cos(2*t)'; '-2/(0.5+t)' '0'};
@@ -10,15 +9,10 @@ clear P1;
   firstSUBounds.center = {'sin(t)'; 'cos(t)'};
   firstSUBounds.center = {'1'; '-1'};
   firstSUBounds.shape = [1 0; 0 1];
-%   secondSUBOunds.center = {'0'};
-%   secondSUBOunds.shape  = 1;
   SVBounds = ellipsoid(1);
   import elltool.conf.Properties;
   timeVec        = [0 5];
-%   phi      = 0:0.1:pi;
-%   dirsMat       = [cos(phi); sin(phi)];
   dirsMat       = [1 0; 0 1; 1 1; -1 1]';
-  %L0       = [1 0]';
   x0EllObj       = ell_unitball(2);
 
   firstSys      = elltool.linsys.LinSysContinuous(thirdAMat, firstBMat, firstSUBounds);
@@ -28,16 +22,16 @@ clear P1;
   rsObj       = elltool.reach.ReachContinuous(firstSys, x0EllObj, dirsMat, timeVec, 'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-3);
 
   rsObj.plotByEa(); hold on;
-%  plotByIa(rs); hold on;
+ plotByIa(rs); hold on;
 
   rsObj = rsObj.evolve(10, secondSys);
 
   rsObj.plotByEa('r'); hold on;
-%  plotByIa(rs, 'y'); hold on;
+ plotByIa(rs, 'y'); hold on;
 	  
   rsObj = rsObj.evolve(15, thirdSys); %problem with regularization
 
   rsObj.plotByEa('g'); hold on;
-%  plotByIa(rs, 'c'); hold on;
+  plotByIa(rs, 'c'); hold on;
 
 end
