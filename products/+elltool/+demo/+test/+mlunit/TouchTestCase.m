f2fe305257069f103bd5ef743983de420a1ff44d
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-03-12 08:37:06 +0400

    issue 79: TouchTestCase.m

--- products/+elltool/+demo/+test/+mlunit/TouchTestCase.m
+++ products/+elltool/+demo/+test/+mlunit/TouchTestCase.m
@@ -10,41 +10,38 @@ classdef TouchTestCase < mlunitext.test_case
             import modgen.io.TmpDataManager;
             import elltool.demo.test.control.*
             testFileName = modgen.common.getcallername(1);
-%             [pathstrVec, ~, ~] = fileparts(which(testFileName));
-%             pathstrVec = [modgen.path.rmlastnpathparts(pathstrVec, 1),filesep,'+control'];
+            [pathstrVec, ~, ~] = fileparts(which(testFileName));
+            pathstrVec = [modgen.path.rmlastnpathparts(pathstrVec, 1),filesep,'+control'];
 %             TmpDataManager.setRootDir(pathstrVec);
-%             fileNameSArray = dir(pathstrVec);
-%             fileNameSArray = fileNameSArray(3:end);
-%             for nameIterator = 1 : size(fileNameSArray,1)
-%                 testName = modgen.string.splitpart...
-%                     (fileNameSArray(nameIterator).name, '.', 'first');
-%                 fTest = str2func(testName);
-%                 if (strcmp(testName,'tt'))
-%                     fTest();
-%                 end
-%             end
-            anim1();
-            anim2();
-            anim3();
-            anim4();
-            anim5();
-            anim6();
-            backdist();
-            bigd();
-            bigd1();
-            bigd();
-            circ();
-            circ1();
-            coll();
-            ctback();
-            ctreach();
-            ctreach3D();
-            ctreach4();
-%             degdist();
-            mech();
-            reachdist();
-            reachdist5();
-            tt();
+            SFileNameArray = dir(pathstrVec);
+            SFileNameArray = SFileNameArray(3:end);
+            for nameIterator = 1 : size(SFileNameArray,1)
+                testName = modgen.string.splitpart...
+                    (SFileNameArray(nameIterator).name, '.', 'first');
+                eval(testName);
+            end
+% % % %             anim1();
+% % % %             anim2();
+% % % %             anim3();
+% % % %             anim4();
+% % % %             anim5();
+% % % %             anim6();
+% % % %             backdist();
+% % % %             bigd();
+% % % %             bigd1();
+% % % %             bigd();
+% % % %             circ();
+% % % %             circ1();
+% % % %             coll();
+% % % %             ctback();
+% % % %             ctreach();
+% % % %             ctreach3D();
+% % % %             ctreach4();
+% % % % %             degdist();
+% % % %             mech();
+% % % %             reachdist();
+% % % %             reachdist5();
+% % % %             tt();
             close all;
             mlunitext.assert(true);
         end
