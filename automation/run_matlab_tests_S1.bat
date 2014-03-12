4e3fe8306b65efa9d0e972c68d00bb7c52ffb52d
pgagarinov <pgagarinov@gmail.com>
2014-03-13 00:26:04 +0400

    Switching to Matlab2013b

--- automation/run_matlab_tests_S1.bat
+++ automation/run_matlab_tests_S1.bat
@@ -7,6 +7,6 @@ echo ===== run_tests_remotely started: %date% %time% =====
 set gitRoot=C:\GIT_Local\ellipsoids_master
 set matlabDir=install
 set logDir=automation\log
-set matlabBin="C:\Program Files (x86)\MATLAB\R2012a\bin"
+set matlabBin="C:\Program Files\MATLAB\R2013b\bin"
 set runMarker=master
 call %~dp0run_matlab_tests.bat %gitRoot% %matlabDir% %logDir% %matlabBin% %runMarker%
