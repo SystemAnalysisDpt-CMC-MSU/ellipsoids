741548dca817969f1ae0f0cd37cd6e3988306a39
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-03-12 16:59:09 +0400

    issue 79: tt.m

--- products/+elltool/+demo/+test/+control/tt.m
+++ products/+elltool/+demo/+test/+control/tt.m
@@ -3,16 +3,12 @@ function tt
 
   aCMat        = {'2' '0'; '0' 't'};
   bCMat        = {'0' 'exp(t)'; 'sqrt(t)' '0'};
-  SUBounds = ell_unitball(2);
-  import elltool.conf.Properties;
+  uBoundsEllObj = ell_unitball(2);
   timeVec        = [1.1 2];
-%   phi      = 0:0.1:pi;
-%   dirsMat       = [cos(phi); sin(phi)];
   dirsMat       = [1 0; 0 1; 1 1; -1 1]';
-  %L0       = [1 1; 1 -1]';
   x0EllObj       = ell_unitball(2) + [1; -1];
 
-  sys      = elltool.linsys.LinSysContinuous(aCMat, bCMat, SUBounds);
+  sys      = elltool.linsys.LinSysContinuous(aCMat, bCMat, uBoundsEllObj);
   rsObj       = elltool.reach.ReachContinuous(sys, x0EllObj, dirsMat, timeVec);
 
   rsObj.plotByEa(); hold on;
