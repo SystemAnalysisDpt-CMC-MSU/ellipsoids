c11392fdbe5927f95eb36d66c05c9507e9b561e9
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-03-12 11:42:10 +0400

    issue 79: bign.m

--- products/+elltool/+demo/+test/+control/bign.m
+++ products/+elltool/+demo/+test/+control/bign.m
@@ -5,8 +5,8 @@ import elltool.conf.Properties;
 
 Properties.setIsVerbose(false);
 Properties.setNTimeGridPoints(135);
-profile clear
-profile on
+% profile clear
+% profile on
 
 test5
 test10
@@ -14,7 +14,7 @@ test25
 test35
 test40
 
-profile viewer
+% profile viewer
 
 % Separate functions for each N are needed for profiling purposes
 function test5
@@ -32,27 +32,27 @@ test(35);
 function test40
 test(40);
 
-function test(dirsNumber)
+function test(N)
 
 time0 = now;
 
-dirsNumber = 5;     % number of directions
+nDirs = 5;     % number of directions
 
-n = 2*dirsNumber;
+n = 2*N;
 
 % System
 aMat = zeros(n, n);
-aMat(1:dirsNumber, dirsNumber+1:end) = eye(dirsNumber);
-onesVec = ones(1, dirsNumber-1);
-aMat(dirsNumber+1:end, 1:dirsNumber) = -diag([2*onesVec, 1]) + diag(onesVec, 1) + diag(onesVec, -1);
-bMat = [zeros(2*dirsNumber-1, 1); 1];
+aMat(1:nDirs, end-nDirs+1:end) = eye(nDirs);
+onesVec = ones(1, nDirs-1);
+aMat(nDirs+1:end, 1:nDirs) = -diag([2*onesVec, 1]) + diag(onesVec, 1) + diag(onesVec, -1);
+bMat = [zeros(2*nDirs-1, 1); 1];
 SUBounds = ellipsoid(1, 1);
 
 sys = elltool.linsys.LinSysContinuous(aMat, bMat, SUBounds);
 
 % Reach set
 x0EllObj = ell_unitball(n);
-dirsMat = randn(n, NL);
+dirsMat = randn(n, nDirs);
 elltool.reach.ReachContinuous(sys, x0EllObj, dirsMat, [-10 0],'isRegEnabled', true, 'isJustCheck', false, 'regTol', 1e-4);
 
-disp(sprintf('Passed test for N = %d: %.1f s', dirsNumber, (now-time0)*86400));
+disp(sprintf('Passed test for N = %d: %.1f s', nDirs, (now-time0)*86400));
