2d607a07aabb400612a9276d9954ed784b7f83fc
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-03-12 16:57:51 +0400

    issue 79: reachdist5.m

--- products/+elltool/+demo/+test/+control/reachdist5.m
+++ products/+elltool/+demo/+test/+control/reachdist5.m
@@ -6,18 +6,14 @@ function reachdist5
 	'0' '0' '-cos(2*t)' '0' '0'
 	'0' '0' '0' '0' '1.5'
 	'0' '0' '0' '-1' '0'};
-  %A  = [0 1 0 0 0; -w 0 0 0 0; 0 0 -0.5 0 0; 0 0 0 0 1.5; 0 0 0 -1 0];
   bMat  = eye(5);
   cMat  = eye(5);
-  SUBounds  = ellipsoid([9 0 0 0 0; 0 2 0 0 0; 0 0 4 0 0; 0 0 0 4 0; 0 0 0 0 9]);
-  SVBounds  = ellipsoid([1 0 0 0 0; 0 2 0 0 0; 0 0 3 0 0; 0 0 0 2 0; 0 0 0 0 1]);
-%   o.save_all = 1;
-  sys  = elltool.linsys.LinSysContinuous(aCMat, bMat, SUBounds, cMat, SVBounds);
+  uBoundsEllObj  = ellipsoid([9 0 0 0 0; 0 2 0 0 0; 0 0 4 0 0; 0 0 0 4 0; 0 0 0 0 9]);
+  vBoundsEllObj  = ellipsoid([1 0 0 0 0; 0 2 0 0 0; 0 0 3 0 0; 0 0 0 2 0; 0 0 0 0 1]);
+  sys  = elltool.linsys.LinSysContinuous(aCMat, bMat, uBoundsEllObj, cMat, vBoundsEllObj);
   
   x0EllObj = ellipsoid([10; -1; 1; 0; -10], 36*eye(5));
-%   dirsMat = [0 1 -1 0 1; 0 1 1 0 0; 1 0 0 1 -1]';
   dirsMat = [0 1 -1 0 1; 0 1 1 0 0]';
-%   o.save_all = 1;
   rsObj = elltool.reach.ReachContinuous(sys, x0EllObj, dirsMat, timeVec,'isRegEnabled',true, 'isJustCheck', false ,'regTol',1e-3);
 
 end
