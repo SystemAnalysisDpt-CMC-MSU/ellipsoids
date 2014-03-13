f3e371ef962de34c5493ac67cc6e3e4dd7964009
Dmitry Kanatnikov <dkanatnikov@gmail.com>
2014-03-13 14:20:37 +0400

    issue 79: small fix

--- products/+elltool/+demo/+test/+control/dtdist2.m
+++ products/+elltool/+demo/+test/+control/dtdist2.m
@@ -10,9 +10,7 @@ d=0;
 cSys1=ss(aMat,b1Mat,cVec,d); 
 cSys2=ss(aMat,b2Mat,cVec,d); 
 dSys1=c2d(cSys1,dt); 
-dSys2=c2d(cSys2,dt); 
-sysStruct1=mpt_sys(dSys1); 
-sysStruct2=mpt_sys(dSys2); 
+dSys2=c2d(cSys2,dt);  
 eAMat = dSys1.a;
 eBMat = dSys1.b;
 eB2Mat = dSys2.b;
@@ -20,7 +18,6 @@ eB2Mat = dSys2.b;
 x0EllObj = ell_unitball(2);
 
 uEllObj = ell_unitball(2);
-u3EllObj = ell_unitball(1);
 vEllObj = ell_unitball(2);
 
 sys = elltool.linsys.LinSysDiscrete(eAMat, eBMat, uEllObj, eB2Mat, vEllObj, [], [], 'd');
