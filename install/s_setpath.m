2346057d413efb7a35c7dea4eb4e080803b42876
pgagarinov <heartofmars@gmail.com>
2015-03-02 23:06:46 +0300

    Pathdef.m is now automatically saved in install subfolder

--- install/s_setpath.m
+++ install/s_setpath.m
@@ -1,9 +1,10 @@
-% rootPathList{2}=[rmlastnpathparts(fileparts(which(mfilename)),2),filesep,'Folder1'];
-rootPathList{1}=rmlastnpathparts(fileparts(which(mfilename)),1);
-pathPatternToExclude='\.svn';
+repoPath=rmlastnpathparts(fileparts(which(mfilename)),1);
+deploymentPath=[repoPath,filesep,'install'];
+%
+rootPathList{1}=repoPath;
+pathPatternToExclude='\.git';
 pathList=genpathexclusive(rootPathList,pathPatternToExclude);
 restoredefaultpath;
 addpath(pathList{:});
 savepath;
-
-
+savepath([deploymentPath,filesep,'pathdef.m']);
