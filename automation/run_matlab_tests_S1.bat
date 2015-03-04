854bb65de4b6111729ee0503198bd73e716e3453
pgagarinov <pgagarinov@gmail.com>
2015-03-04 05:56:49 +0300

    Relocation of test automation repo

--- automation/run_matlab_tests_S1.bat
+++ automation/run_matlab_tests_S1.bat
@@ -4,7 +4,7 @@ setlocal
 
 echo ===== run_tests_remotely started: %date% %time% =====
 
-set gitRoot=C:\GIT_Local\ellipsoids_master
+set gitRoot=C:\GIT_Local\ellipsoids_master\ellipsoids
 set matlabDir=install
 set logDir=automation\log
 set matlabBin="C:\Program Files\MATLAB\R2013b\bin"
