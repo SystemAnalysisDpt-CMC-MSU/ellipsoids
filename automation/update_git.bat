e22b49b49e1e76f91bd10aceae785a0d98eb221e
pgagarinov <pgagarinov@gmail.com>
2015-04-15 22:18:53 +0300

    Minor bug fix

--- automation/update_git.bat
+++ automation/update_git.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
-:: Update SVN tree
+:: Update GIT repository
 :: Parameters:
-:: 1. svn_dir - string: location of the SVN trunk local copy
+:: 1. git_dir - string: location of the SVN trunk local copy
 
 if "%~1"=="" (
 	echo %0: Too few parameters 1>&2
@@ -15,5 +15,5 @@ cd %repo_dir%
 @echo off
 for /f %%i in ('git rev-parse --abbrev-ref --symbolic-full-name @{u}') do set remote_branch_name=%%i
 @echo on
+git fetch
 git reset --hard %remote_branch_name%
-:: --non-interactive
