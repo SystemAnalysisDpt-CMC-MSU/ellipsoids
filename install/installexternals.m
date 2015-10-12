381f495d6aa1d5d372f67c577f1c471000173c21
Peter Gagarinov <pgagarinov@gmail.com>
2015-10-12 22:48:08 +0300

    issue #20 Enhancement: implemented a script for automatic installation of externals: CVX and MPT

---
+++ install/installexternals.m
@@ -0,0 +1,51 @@
+function installexternals(isSkippedIfExists)
+if nargin<1
+	isSkippedIfExists=false;
+end
+%
+curDir=fileparts(which(mfilename));
+extFolder=[curDir,filesep,'..',filesep,'externals'];
+if exist(extFolder,'dir')
+    if isSkippedIfExists
+        return;
+    end
+    while true
+        promptStr=sprintf(...
+            ['Directory %s already exists.\n\t\t\tIf you proceed this ',...
+            'folders''s content will be completely overwritten.\n\t\t\t',...
+            'Do you want to proceed? [y]/n: '],...
+            strrep(extFolder,filesep,[filesep,filesep]));
+        str = input(promptStr,'s');
+        if isempty(str)
+            str = 'y';
+        end
+        switch lower(str)
+            case 'y'
+                fprintf('Deleting directory\n\t\t\t%s...',extFolder);
+                rmdir(extFolder,'s');
+                fprintf('done\n');
+                break;
+            case 'n';
+                break;
+            otherwise
+                sprintf('Unrecognized input');
+                continue;
+        end
+    end
+end
+EXTERNALS_URL=['https://github.com/SystemAnalysisDpt-CMC-MSU/',...
+    'ellipsoids/releases/download/',...
+    '2.1/cvx2.1_b1110_mpt3_1_win32_and_win64.zip'];
+tmpFile=[curDir,filesep,'externals.zip'];
+fprintf('Downloading externals from\n\t\t\t%s\n\t\t\tinto\n\t\t\t%s...',...
+    EXTERNALS_URL,tmpFile);
+urlwrite(EXTERNALS_URL,tmpFile);
+fprintf('done\n');
+msgStr=sprintf('Unpacking externals into\n\t\t\t%s...',...
+    strrep(extFolder,filesep,[filesep,filesep]));
+fprintf(msgStr);
+unzip(tmpFile,[curDir,filesep,'..',filesep,'externals']);
+fprintf('done\n');
+fprintf('Deleting temporary file\n\t\t\t %s...',tmpFile);
+delete(tmpFile);
+fprintf('done\n');
--- install/s_install.m
+++ install/s_install.m
@@ -1,5 +1,6 @@
 welcomeString=sprintf('Installing Ellipsoidal Toolbox');
 disp([welcomeString,'...']);
+installexternals(true);
 warning('on','all');
 warning('off','MATLAB:dispatcher:pathWarning');
 s_setpath;
@@ -7,6 +8,6 @@ s_setjavapath;
 modgen.logging.log4j.Log4jConfigurator.configureSimply();
 %
 %% Configure CVX is needed
-ellipsoids_init();
+ellipsoidsinit();
 disp([welcomeString,': done']);
 
