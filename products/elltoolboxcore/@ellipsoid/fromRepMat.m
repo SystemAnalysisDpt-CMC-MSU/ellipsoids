5bd67ef0b9c12760244bb194f60f9140af9019de
pgagarinov <pgagarinov@gmail.com>
2015-11-11 13:58:31 +0300

    BugFix: minor changes to fromRepMat.m help header

--- products/elltoolboxcore/@ellipsoid/fromRepMat.m
+++ products/elltoolboxcore/@ellipsoid/fromRepMat.m
@@ -1,51 +1,52 @@
 function ellArr = fromRepMat(varargin)
 % FROMREPMAT - returns array of equal ellipsoids the same
-%              size as stated in sizeVec argument
-%
-%   ellArr = fromRepMat(sizeVec) - creates an array  size 
-%            sizeVec of empty ellipsoids.
-%   
-%   ellArr = fromRepMat(shMat,sizeVec) - creates an array  
-%            size sizeVec of ellipsoids with shape matrix 
-%            shMat.
-%   
-%   ellArr = fromRepMat(cVec,shMat,sizeVec) - creates an
-%            array size sizeVec of ellipsoids with shape 
-%            matrix shMat and center cVec.
-%
+% 		size as stated in sizeVec argument
+% 
+% ellArr = fromRepMat(sizeVec) - creates an array of size 
+%   sizeVec of empty ellipsoids.
+% 
+% ellArr = fromRepMat(shMat,sizeVec) - creates an array of size 
+%   sizeVec of ellipsoids with shape matrix shMat.
+% 
+% ellArr = fromRepMat(cVec,shMat,sizeVec) - creates an
+%   array of size sizeVec of ellipsoids with shape 
+%   matrix shMat and center cVec.
+% 
 % Input:
 %   Case1:
 %       regular: 
 %           sizeVec: double[1,n] - vector of size, have 
-%           integer values.
-%
+%               integer values.
+% 
 %   Case2:
 %       regular:
 %           shMat: double[nDim, nDim] - shape matrix of 
-%           ellipsoids. 
+%               ellipsoids. 
 %           sizeVec: double[1,n] - vector of size, have 
-%           integer values.
-%
+%               integer values.
+% 
 %   Case3:
 %       regular:
 %           cVec: double[nDim,1] - center vector of 
-%           ellipsoids
+%               ellipsoids
 %           shMat: double[nDim, nDim] - shape matrix of 
-%           ellipsoids. 
+%               ellipsoids. 
 %           sizeVec: double[1,n] - vector of size, have 
-%           integer values.
-%
-%   properties:
-%       absTol: double [1,1] - absolute tolerance with default
-%           value 10^(-7)
-%       relTol: double [1,1] - relative tolerance with default
-%           value 10^(-5)
-%       nPlot2dPoints: double [1,1] - number of points for 2D plot
-%           with default value 200
-%       nPlot3dPoints: double [1,1] - number of points for 3D plot
-%           with default value 200.
-%
-% $Author: <Zakharov Eugene>  <justenterrr@gmail.com> $ 
+%               integer values.
+% 
+% properties:
+% 	absTol: double [1,1] - absolute tolerance with default
+% 		value 10^(-7)
+% 	relTol: double [1,1] - relative tolerance with default
+% 		value 10^(-5)
+% 	nPlot2dPoints: double [1,1] - number of points for 2D plot
+% 		with default value 200
+% 	nPlot3dPoints: double [1,1] - number of points for 3D plot
+% 		with default value 200.
+% Output:
+%   ellArr: ellipsoid[] - created ellipsoidal array
+%   
+% $Author: <Zakharov Eugene>	<justenterrr@gmail.com> $ 
 % $Date: <april> $
 % $Copyright: Moscow State University,
 % Faculty of Computational Mathematics and 
@@ -55,20 +56,20 @@ function ellArr = fromRepMat(varargin)
 import modgen.common.checkvar;
 %
 if nargin > 3
-    indVec = [1:2,4:nargin];
-    sizeVec = varargin{3};
+	indVec = [1:2,4:nargin];
+	sizeVec = varargin{3};
 else
-    sizeVec = varargin{nargin};
-    indVec = [1:nargin-1];
+	sizeVec = varargin{nargin};
+	indVec = [1:nargin-1];
 end
 %
 if ~isa(sizeVec,'double')
-    modgen.common.throwerror('wrongInput','Size array is not double');
+	modgen.common.throwerror('wrongInput','Size array is not double');
 end
 sizeVec = gras.la.trytreatasreal(sizeVec);
 checkvar(sizeVec,@(x) all(mod(x(:),1) == 0) && all(x(:) > 0) ...
-    && (size(x,1) == 1), 'errorTag', 'wrongInput', ...
-    'errorMessage','size array must contain positive integer values.');
+	&& (size(x,1) == 1), 'errorTag', 'wrongInput', ...
+	'errorMessage','size array must contain positive integer values.');
 %
 nEllipsoids = prod(sizeVec);
 ellArr(nEllipsoids) = ellipsoid();
@@ -78,7 +79,6 @@ arrayfun(@(x)makeEllipsoid(x),1:nEllipsoids);
 ellArr = reshape(ellArr,sizeVec);
 %
 function makeEllipsoid(index)
-    ellArr(index) = getCopy(ell);
+	ellArr(index) = getCopy(ell);
 end
 end
-        
