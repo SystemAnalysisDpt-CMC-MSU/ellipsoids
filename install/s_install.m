d530965b572c1db17453bbb1410ffe187a4fa124
Ilya Roublev <irublev@alliedtesting.com>
2017-11-19 19:27:43 +0300

    issue #59: Installing script switches off deprecation messages for nargchk in the case of Matlab R2016a

--- install/s_install.m
+++ install/s_install.m
@@ -3,6 +3,11 @@ disp([welcomeString,'...']);
 installexternals(true);
 warning('on','all');
 warning('off','MATLAB:dispatcher:pathWarning');
+% switch off warnings for nargchk for Matlab R2016a
+SMatlabInfo=ver('Matlab');
+if strcmp(SMatlabInfo.Release,'(R2016a)')
+    warning('off','MATLAB:nargchk:deprecated');
+end
 s_setpath;
 s_setjavapath;
 modgen.logging.log4j.Log4jConfigurator.configureSimply();
