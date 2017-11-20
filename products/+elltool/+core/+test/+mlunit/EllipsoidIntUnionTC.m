9c8b8d5729c270146e7f87dea70339af5191dbbf
Ilya Roublev <irublev@alliedtesting.com>
2017-11-20 23:04:51 +0300

    issue #59: Adjusted test for sensitivities of ellipsoids union

--- products/+elltool/+core/+test/+mlunit/EllipsoidIntUnionTC.m
+++ products/+elltool/+core/+test/+mlunit/EllipsoidIntUnionTC.m
@@ -34,7 +34,7 @@ classdef EllipsoidIntUnionTC < mlunitext.test_case
             import elltool.conf.Properties;
             self.setUpCheckSettings();
             relTol = Properties.getAbsTol();
-            sensEPS = 0.005*relTol;
+            sensEPS = 0.0001*relTol;
             load(strcat(self.testDataRootDir, strcat(filesep,...
                 'testEllunionEa_inp.mat')), 'testEllCenterVec', ...
                 'testEllMat', 'testEllCenter2Vec', 'testEll2Mat');
