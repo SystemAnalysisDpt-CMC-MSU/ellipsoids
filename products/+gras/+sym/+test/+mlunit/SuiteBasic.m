9e5c8444d524a9f55219d0edba702c1a70546b7c
NikolayTrusov <trunick.10.96@gmail.com>
2017-12-18 14:34:53 +0300

    issue #50: Enhancement: improved tests in SuiteBasic.m, modified varreplace.m

--- products/+gras/+sym/+test/+mlunit/SuiteBasic.m
+++ products/+gras/+sym/+test/+mlunit/SuiteBasic.m
@@ -1,146 +1,138 @@
 classdef SuiteBasic < mlunitext.test_case
-    properties
-    end
-
+    
     methods
         function self = SuiteBasic(varargin)
             self = self@mlunitext.test_case(varargin{:});
         end
-    
-
-
-        function self = set_up_param(self,varargin)
-
-        end
-
+        
         function testVarreplaceInt(~)
             mCMat={'t+t^2+t^3+sin(t)', 't^(1/2)+t*t*17';...
-                   'att+t2', 't+temp^t';...
-                   '1/(t+3)*2^t^t', 't-t^t'};
-
+                'att+t2', 't+temp^t';...
+                '1/(t+3)*2^t^t', 't-t^t'};
+            
             fromVarName = 't';
             tSum = '15';
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'(15-t)+(15-t)^2+(15-t)^3+sin((15-t))', '(15-t)^(1/2)+(15-t)*(15-t)*17';...
-                     'att+t2',                               '(15-t)+temp^(15-t)';...
-                     '1/((15-t)+3)*2^(15-t)^(15-t)',         '(15-t)-(15-t)^(15-t)'};
+                'att+t2',                               '(15-t)+temp^(15-t)';...
+                '1/((15-t)+3)*2^(15-t)^(15-t)',         '(15-t)-(15-t)^(15-t)'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
-  
+        
         function testVarreplaceReal(~)
             mCMat={'t+t^2+t^3+sin(t)', 't^(1/2)+t*t*17';...
-                   'att+t2',           't+temp^t';...
-                   '1/(t+3)*2^t^t',    't-t^t'};
-
+                'att+t2',           't+temp^t';...
+                '1/(t+3)*2^t^t',    't-t^t'};
+            
             fromVarName = 'att';
             tSum = '10.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'t+t^2+t^3+sin(t)', 't^(1/2)+t*t*17';...
-                    '(10.8-att)+t2',    't+temp^t';...
-                    '1/(t+3)*2^t^t',    't-t^t'};
+                '(10.8-att)+t2',    't+temp^t';...
+                '1/(t+3)*2^t^t',    't-t^t'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
-
+        
         function testVarreplaceSpaces(~)
             mCMat={'t +      t^2+t^3+sin(t)', 't ^( 1/2)+t*t *17';...
                    'att +t2',                 't+temp^t';...
                    '1/(t+3)*2^t^t',           't -t^ t'};
-
+            
             fromVarName = 't';
             tSum = '15';
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'(15-t)+(15-t)^2+(15-t)^3+sin((15-t))', '(15-t)^(1/2)+(15-t)*(15-t)*17';...
-                    'att+t2',                               '(15-t)+temp^(15-t)';...
-                    '1/((15-t)+3)*2^(15-t)^(15-t)',         '(15-t)-(15-t)^(15-t)'};
+                'att+t2',                                    '(15-t)+temp^(15-t)';...
+                '1/((15-t)+3)*2^(15-t)^(15-t)',              '(15-t)-(15-t)^(15-t)'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
-
+        
         function testVarreplaceCombOfVars(~)
             mCMat={'t+tt+   ttt',     't^(t/2)+t*tt-t';...
-                   'atttt+ttt2',      't+temp^t+0.1';...
-                   '1/(t+3)*2^tt^ts', 'st-t^t'};
-
+                'atttt+ttt2',      't+temp^t+0.1';...
+                '1/(t+3)*2^tt^ts', 'st-t^t'};
+            
             fromVarName = 't';
             tSum = '0.01';
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'(0.01-t)+tt+ttt',        '(0.01-t)^((0.01-t)/2)+(0.01-t)*tt-(0.01-t)';...
-                     'atttt+ttt2',             '(0.01-t)+temp^(0.01-t)+0.1';...
-                     '1/((0.01-t)+3)*2^tt^ts', 'st-(0.01-t)^(0.01-t)'};
+                'atttt+ttt2',             '(0.01-t)+temp^(0.01-t)+0.1';...
+                '1/((0.01-t)+3)*2^tt^ts', 'st-(0.01-t)^(0.01-t)'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
-
+        
         function testVarreplaceVec(~)
             mCMat={'tt + t^3+sin(cos(tt))', 't^(1/2)+t*t*17', 'tt'};
-
+            
             fromVarName = 'tt';
             tSum = '0.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'(0.8-tt)+t^3+sin(cos((0.8-tt)))', 't^(1/2)+t*t*17', '(0.8-tt)'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
-
+        
         function testVarreplaceOneElem(~)
             mCMat={'tt'};
-
+            
             fromVarName = 'tt';
             tSum = '0.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'(0.8-tt)'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
-
+        
         function testVarreplaceNoToRepVar(~)
             mCMat={'t'};
-
+            
             fromVarName = 'tt';
             tSum = '0.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'t'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
-
+        
         function testVarreplaceNoToRepFun(~)
             mCMat={'sqrt(tt)'};
-
+            
             fromVarName = 't';
             tSum = '0.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'sqrt(tt)'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
@@ -148,33 +140,33 @@ classdef SuiteBasic < mlunitext.test_case
         
         function testVarreplaceNoToRepMat(~)
             mCMat={'exp(t)',      'sin(ttt)', '-t';...
-                   'tttt',        't_ttt',     '-0.6*t';...
-                   'tan(exp(t))', 'exp(10)', 'cos(t - ttt)'};
-
+                'tttt',        't_ttt',     '-0.6*t';...
+                'tan(exp(t))', 'exp(10)', 'cos(t - ttt)'};
+            
             fromVarName = 'tt';
             tSum = '0.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'exp(t)',      'sin(ttt)', '-t';...
-                     'tttt',        't_ttt',     '-0.6*t';...
-                     'tan(exp(t))', 'exp(10)', 'cos(t-ttt)'};
+                'tttt',        't_ttt',     '-0.6*t';...
+                'tan(exp(t))', 'exp(10)', 'cos(t-ttt)'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
         
         function testVarreplaceOneIntElem(~)
             mCMat={150};
-
+            
             fromVarName = 'att';
             tSum = '10.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'150'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
@@ -182,110 +174,110 @@ classdef SuiteBasic < mlunitext.test_case
         
         function testVarreplaceMatInt(~)
             mCMat={150, 1,  2;
-                   -10, 30, 100;
-                   1,   0,  -190};
-
+                -10, 30, 100;
+                1,   0,  -190};
+            
             fromVarName = 'att';
             tSum = '10.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'150', '1',  '2';
-                     '-10', '30', '100';
-                     '1',   '0',  '-190'};
+                '-10', '30', '100';
+                '1',   '0',  '-190'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
         
         function testVarreplaceOneRealElem(~)
             mCMat={0.01};
-
+            
             fromVarName = 'att';
             tSum = '10.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
-            corCMat={'0.01'};
+            
+            corCMat={'0.010000000000000000208'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
         
         function testVarreplaceMatreal(~)
             mCMat={150.1, 1.08,  2.09;
-                   -10.008, 30.1, 100.01;
-                   1.1,   0.556,  -190.901};
-
+                -10.008, 30.1, 100.01;
+                1.1,   0.556,  -190.901};
+            
             fromVarName = 'att';
             tSum = '10.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
-            corCMat={'150.1',   '1.08',  '2.09';
-                     '-10.008', '30.1',  '100.01';
-                     '1.1',     '0.556', '-190.901'};
+            
+            corCMat={'150.09999999999999432',   '1.0800000000000000711',  '2.0899999999999998579';
+                '-10.007999999999999119', '30.100000000000001421',  '100.01000000000000512';
+                '1.1000000000000000888',     '0.55600000000000004974', '-190.90100000000001046'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
         
         function testVarreplaceIntElem(~)
             mCMat={'t+t^2+t^3+sin(t)', 't^(1/2)+t*t*17';...
-                   'att+t2',           't+temp^t';...
-                   '1/(t+3)*2^t^t',     1};
-
+                'att+t2',           't+temp^t';...
+                '1/(t+3)*2^t^t',     1};
+            
             fromVarName = 'att';
             tSum = '10.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'t+t^2+t^3+sin(t)', 't^(1/2)+t*t*17';...
-                     '(10.8-att)+t2',    't+temp^t';...
-                     '1/(t+3)*2^t^t',    '1'};
+                '(10.8-att)+t2',    't+temp^t';...
+                '1/(t+3)*2^t^t',    '1'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
         
         function testVarreplaceRealElem(~)
             mCMat={'3.2',           't';...
-                   'att+t2',        't+temp^t';...
-                   '1/(t+3)*2^t^t', 1.9};
-
+                'att+t2',        't+temp^t';...
+                '1/(t+3)*2^t^t', 1.9};
+            
             fromVarName = 'att';
             tSum = '10.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'3.2',           't';...
-                     '(10.8-att)+t2', 't+temp^t';...
-                     '1/(t+3)*2^t^t', '1.9'};
+                '(10.8-att)+t2', 't+temp^t';...
+                '1/(t+3)*2^t^t', '1.8999999999999999112'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
         
         function testVarreplaceMixedElems(~)
             mCMat={'3.2',    't + 3.2';...
-                   9.8,      -34;...
-                   'sin(t)', 1.9};
-
+                9.8,      -34;...
+                'sin(t)', 1.9};
+            
             fromVarName = 't';
             tSum = '0.81';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'3.2',         '(0.81-t)+3.2';...
-                     '9.8',         '-34';...
-                   'sin((0.81-t))', '1.9'};
+                '9.8000000000000007105',         '-34';...
+                'sin((0.81-t))', '1.8999999999999999112'};
             isOk=isequal(corCMat, resCMat);
             mlunitext.assert_equals(true,isOk);
         end
@@ -296,18 +288,18 @@ classdef SuiteBasic < mlunitext.test_case
             end
             syms t;
             mCMat={sin(t), 10;
-                   'cos(2*t)', sin(3*t)};
-               
+                'cos(2*t)', sin(3*t)};
+            
             fromVarName = 't';
             tSum = '1';
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'sin((1-t))', '10';
-                     'cos(2*(1-t))', 'sin(3*(1-t))'};
+                'cos(2*(1-t))', 'sin(3*(1-t))'};
             isOk=isequal(corCMat, resCMat);
-            mlunitext.assert_equals(true,isOk);       
+            mlunitext.assert_equals(true,isOk);
         end
         
         function testVarreplaceAll(~)
@@ -316,42 +308,42 @@ classdef SuiteBasic < mlunitext.test_case
             end
             syms t tt;
             mCMat={sin(t), 10, exp(tt);
-                   'cos(2*t)', sin(3*t), -1.09;
-                   '     3*t*tt', t + tt, 1};
-               
+                'cos(2*t)', sin(3*t), -1.09;
+                '     3*t*tt', t + tt, 1};
+            
             fromVarName = 't';
             tSum = '1';
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName);
             resCMat=gras.sym.varreplace(mCMat, fromVarName, toVarName);
-
+            
             corCMat={'sin((1-t))', '10', 'exp(tt)';
-                     'cos(2*(1-t))', 'sin(3*(1-t))', '-1.09';
-                     '3*(1-t)*tt', '(1-t)+tt', '1'};
+                'cos(2*(1-t))', 'sin(3*(1-t))', '-1.0900000000000000799';
+                '3*(1-t)*tt', '(1-t)+tt', '1'};
             isOk=isequal(corCMat, resCMat);
-            mlunitext.assert_equals(true,isOk);       
+            mlunitext.assert_equals(true,isOk);
         end
-
+        
         function testVarreplaceWrongInArgs(self)
             mCMat={'t+t^2+t^3+sin(t)', 't^(1/2)+t*t*17';...
-                   'att+t2',           't+temp^t';...
-                   '1/(t+3)*2^t^t',    't-t^t'}; %#ok<NASGU>
-
+                'att+t2',           't+temp^t';...
+                '1/(t+3)*2^t^t',    't-t^t'}; %#ok<NASGU>
+            
             fromVarName = 'att';
             tSum = '10.8';
-
+            
             toVarName = strcat(tSum,'-');
             toVarName = strcat(toVarName, fromVarName); %#ok<NASGU>
-
+            
             self.runAndCheckError(...
                 'gras.sym.varreplace(mCMat, fromVarName)',...
                 ':wrongInput');
         end
-
-
+        
+        
         function testVarreplaceEmptMat(self)
             mCMat={}; %#ok<NASGU>
-
+            
             fromVarName = 't';
             tSum = '0.01';
             toVarName = strcat(tSum,'-');
--- products/+gras/+sym/varreplace.m
+++ products/+gras/+sym/varreplace.m
@@ -1,22 +1,34 @@
-function reverseCMat=varreplace(mCMat,fromVarName,toVarName)
-% VARREPLACE allows to solve the system set by
-% cell matrix mCMat in the reverse time 
+function resCMat=varreplace(mCMat,fromVarName,toVarName)
+% VARREPLACE allows to change an independent variable to some 
+% expression in a cell matrix mCMat containing a bunch of expressions
 % by replacing of variable fromVarName to toVarName
 %
 % Input:
 % 	regular:
 % 		mCMat: cell[nDims1,nDims2] -
-% 			cell matrix of system
+%          cell matrix of expressions; each cell may be either a number,
+%          a char string or a symbolic expression (the latter in the case
+%          when Symbolic Toolbox is installed); it should be noted that 
+%          numbers are transformed into strings using 20 signs after 
+%          the decimal point to preserve accuracy, so the result may look
+%          a bit confused
 %       fromVarName: variable that we want to be replaced
 %       toVarName: expression that we get instead of variable fromVarName
 %
 % Output:
-% 	reverseCMat: cell[nDims1,nDims2] -
-% 		cell matrix of system reflected 
-% 		about the expression toVarName
+% 	resCMat: char cell[nDims1,nDims2] -
+% 		cell matrix of expressions after the made changes
+% Example:
+%   resCMat = varreplace({'3.2',   't + 3.2';...
+%                          9.8,     -34;...
+%                         'sin(t)', 1.9},'t','(0.81-t)')
+%
+% resCMat = {'3.2',                   '(0.81-t)+3.2';...
+%            '9.8000000000000007105', '-34';...
+%            'sin((0.81-t))',         '1.8999999999999999112'}
 %
 %
-% $Author: Nikolay Trusov  <trunick.10.96@gmail.com>$	$Date: 2017-12-16$
+% $Author: Nikolay Trusov  <trunick.10.96@gmail.com>$	$Date: 2017-12-18$
 % $Copyright: Moscow State University,
 % 			Faculty of Computational Mathematics and Computer Science,
 % 			System Analysis Department 2017$
@@ -26,34 +38,37 @@ function reverseCMat=varreplace(mCMat,fromVarName,toVarName)
 if(nargin ~= 3)
     modgen.common.throwerror('wrongInput',...
         'Not Enough Arguments to call the function varreplace');
-elseif (isempty(mCMat))
-    modgen.common.throwerror('wrongInput',...
-        'mCMat must not be empty');
-elseif (~ischar(fromVarName))
-    modgen.common.throwerror('wrongInput',...
-        'fromVarName is expected to be a string');
-elseif (~ischar(toVarName))
-    modgen.common.throwerror('wrongInput',...
-        'toVarName is expected to be a string');
-else
-    isSymMat = cellfun(@(x)isa(x,'sym'),mCMat);
-    mCMat(isSymMat) = cellfun(@char,mCMat(isSymMat),'UniformOutput',false);
-    isnCharMat = ~cellfun('isclass',mCMat,'char');
-    mCMat(isnCharMat)=cellfun(@(x)num2str(x),mCMat(isnCharMat),'UniformOutput',false);
-    mCMat = strrep(mCMat,' ','');
-    regExpression = strcat('(^',fromVarName);
-    regExpression = strcat(regExpression,'\>|\<');
-    regExpression = strcat(regExpression,fromVarName);
-    regExpression = strcat(regExpression,'\>|^');
-    regExpression = strcat(regExpression,fromVarName);
-    regExpression = strcat(regExpression,'$|\<');
-    regExpression = strcat(regExpression,fromVarName);
-    regExpression = strcat(regExpression,'$)');
-    repStr = strcat('(',toVarName);
-    repStr = strcat(repStr,')');
-
-    reverseCMat = cellfun(@(X) regexprep(X,regExpression,repStr), mCMat,'UniformOutput',false);
 end
+
+modgen.common.checkvar(mCMat,'~isempty(x)&&iscell(x)',...
+    'errorTag','wrongInput','errorMessage','mCMat must not be empty');
+modgen.common.checkvar(fromVarName,'ischar(x)',...
+    'errorTag','wrongInput','errorMessage',...
+    'fromVarName is expected to be a string');
+modgen.common.checkvar(toVarName,'ischar(x)',...
+    'errorTag','wrongInput','errorMessage',...
+    'toVarName is expected to be a string');
+
+
+isSymMat = cellfun(@(x)isa(x,'sym'),mCMat);
+mCMat(isSymMat) = cellfun(@char,mCMat(isSymMat),'UniformOutput',false);
+isnCharMat = ~cellfun('isclass',mCMat,'char');
+mCMat(isnCharMat)=cellfun(@(x)num2str(x,20),mCMat(isnCharMat),... 
+    'UniformOutput',false);
+mCMat = strrep(mCMat,' ','');
+regExpression = strcat('(^',fromVarName);
+regExpression = strcat(regExpression,'\>|\<');
+regExpression = strcat(regExpression,fromVarName);
+regExpression = strcat(regExpression,'\>|^');
+regExpression = strcat(regExpression,fromVarName);
+regExpression = strcat(regExpression,'$|\<');
+regExpression = strcat(regExpression,fromVarName);
+regExpression = strcat(regExpression,'$)');
+repStr = strcat('(',toVarName);
+repStr = strcat(repStr,')');
+
+resCMat = cellfun(@(X) regexprep(X,regExpression,repStr),...
+    mCMat,'UniformOutput',false);
 
 
 
