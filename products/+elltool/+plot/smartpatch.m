f5f7a1b5827bdb0e6a490a6e285669da2d621a80
Stanislav Mologin <stas.mologin@gmail.com>
2017-12-23 20:57:32 +0300

    issue #51: Added description for smartpatch

--- products/+elltool/+plot/smartpatch.m
+++ products/+elltool/+plot/smartpatch.m
@@ -1,10 +1,19 @@
 function hPatch = smartpatch(isShowLegend, extraPropCVec, varargin)
+% SMARTPATCH patch-wrapper for patch allowing to pass it extra arguments
 %
-% SMARTPATCH legend-friendly wrapper for 'patch'
+% Usage:
+%   hPatch = smartpatch(isShowLegend, extraPropCVec, patchArgs)
 %
-% In order to correct legend displaying, there ...to be continued
-%
-% Usage: 
+% input:
+%       regular:
+%           isShowLegend: logical[1, 1] - display or not legend annotation for
+%               this object
+%           extraPropCVec: cell[1, ] - cell array of extra patch properties
+%           patchArgs: - arguments to pass directly to patch ('help patch'
+%               for details)
+% output:
+%       regular:
+%           hPatch: matlab.graphics.primitive.Patch[1, 1] - patch handle
 
 hPatch = patch(varargin{:});
 if (~isempty(extraPropCVec))
