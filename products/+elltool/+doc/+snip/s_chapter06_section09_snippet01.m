7c604829c1a388397b5c0abc011e067acd5da5ff
ShaludinAlexandr <alexandr.vmk@gmail.com>
2018-02-24 16:07:21 +0300

    issue #58: Enhancement:   fixed the naming of variables according to Coding policy;   fixed the indents;   deleted unnecessary comments;

--- products/+elltool/+doc/+snip/s_chapter06_section09_snippet01.m
+++ products/+elltool/+doc/+snip/s_chapter06_section09_snippet01.m
@@ -1,109 +1,99 @@
 %%
-% define system
-% initial conditions
-k = 3;
+% define all initial conditions
 delta = 1;
-eps = 1E-5;
-F_10 = 0.015;
-F_20 = 0.005;
-F_0 = 0.02;
-c_1 = 1;
-c_2 = 2;
-c_0 = 1.25;
-V_0 = 1;
-tetha = V_0/F_0;
-uVec = [1, 1]';
+f10 = 0.015;
+f20 = 0.005;
+f0 = 0.02;
+c1 = 1;
+c2 = 2;
+c0 = 1.25;
+v0 = 1;
+
+%theta is the tank filling time
+theta = v0/f0;
+
+%define the error
+error = 1E-5;
+
+%set the number of cycles of delay
+k = 3;
+
+%define the initial vector
 x0Vec = 5*ones(2*k+2, 1);
-x0Vec(1) = V_0;
-x0Vec(2) = c_0;
+x0Vec(1) = v0;
+x0Vec(2) = c0;
 
 %define the certain volume and concentration
-V = 10;
-c = 4;
+v = 10;
+c = 10;
+x1Vec = zeros(2*k+2, 1);
+x1Vec(1) = 0;
+x1Vec(2) = 0;
 
-
+%define the matrixes A' and B' for equation x' = A'x + B'u
 firstAMat = [...
-    exp(-delta/(2*tetha)) 0;
-    0                     exp(-delta/tetha)];
+    exp(-delta/(2*theta)) 0;
+    0                     exp(-delta/theta)];
 
 firstBMat = [...
-    2*tetha*(1 - exp(-delta/(2*tetha))) 2*tetha*(1 - exp(-delta/(2*tetha)));
+    2*theta*(1 - exp(-delta/(2*theta))) 2*theta*(1 - exp(-delta/(2*theta)));
     0                                   0
-];
+    ];
 
 secondBMat = [...
-    0 0;
-    ((tetha*(c_1 - c_0)) / V_0) * (1 - exp(-delta/(tetha))) ((tetha*(c_2 - c_0)) / V_0) * (1 - exp(-delta/(tetha)))
-];
-% 
-% myCAMat = [...
-% firstAMat secondBMat;
-% 0 0 0 0;
-% 0 0 0 0
-% ];
-% 
-% 
-% myCBMat = [...
-%     firstBMat;
-%     eye(2)];
+    0                                                               0;
+    ((theta*(c1 - c0)) / v0) * (1 - exp(-delta/(theta))) ((theta*(c2 - c0)) / v0) * (1 - exp(-delta/(theta)))
+    ];
+
+finalCAMat = zeros(2*k+2);
+finalCAMat(1:2, 1:2) = firstAMat;
+finalCAMat(1:2, 2*k+1:2*k+2) = secondBMat;
+finalCAMat(5:2*k+2, 3:2*k) = eye(2*k-2);
 
 
-myCAMat = zeros(2*k+2);
-myCAMat(1:2, 1:2) = firstAMat;
-myCAMat(1:2, 2*k+1:2*k+2) = secondBMat;
-myCAMat(5:2*k+2, 3:2*k) = eye(2*k-2);
-
-
-myCBMat = zeros(2*k+2, 2);
-myCBMat(1:2, :) = firstBMat;
-myCBMat(3:4, :) = eye(2);
+finalCBMat = zeros(2*k+2, 2);
+finalCBMat(1:2, :) = firstBMat;
+finalCBMat(3:4, :) = eye(2);
 
 %define the timeVec for ReachDiscrete function
 maxTime = 5;
 timeVec = [0, maxTime];
 
 %define bounds for control
-uEllObj = uVec + ell_unitball(2); 
+uVec = [1, 1]';
+uEllObj = uVec + ell_unitball(2);
 
 %define the x0EllObj
-QMat = eps * eye(2*k+2);
-x0EllObj = ellipsoid(x0Vec, QMat);
+qMat = error * eye(2*k+2);
+x0EllObj = ellipsoid(x0Vec, qMat);
 
-%define the system
-linSysObj=elltool.linsys.LinSysDiscrete(myCAMat, myCBMat, uEllObj);
+%define the discrete system
+linSysObj=elltool.linsys.LinSysDiscrete(finalCAMat, finalCBMat, uEllObj);
 
-% % %definition of dirMat
-% nDims = 4; 
-% nDirs = 1; 
-% dirMeshVec = 0:(2*pi/nDirs):2*pi; 
-% dirNum = length(dirMeshVec)^nDims; 
-% [x1, x2, x3, x4] = ndgrid(dirMeshVec,dirMeshVec,dirMeshVec,dirMeshVec); 
-% lineDir1Mat = reshape( x1, [1, dirNum]); 
-% lineDir2Mat = reshape( x2, [1, dirNum]); 
-% lineDir3Mat = reshape( x3, [1, dirNum]); 
-% lineDir4Mat = reshape( x4, [1, dirNum]); 
-% dirMat = [lineDir1Mat; lineDir2Mat; lineDir3Mat; lineDir4Mat]; 
-% dirMat = dirMat(:,2:end); 
-% dirMat = dirMat ./ sqrt(repmat(sum(dirMat.^2,1), nDims, 1)); 
+% definition of dirMat
 dirMat = eye(2*k+2);
 
-
-% solvObj = elltool.reach.ReachDiscrete(linSysObj, x1EllObj, dirMat, ... 
-% timeVec, 'isRegEnabled', true, 'isJustCheck', false, 'regTol', 1e-1);
-solvObj = elltool.reach.ReachDiscrete(linSysObj, x0EllObj, dirMat, ... 
-timeVec, 'isRegEnabled', true, 'isJustCheck', false, 'regTol', 1e-1);
+%construction of reachability tube
+solvObj = elltool.reach.ReachDiscrete(linSysObj, x0EllObj, dirMat, ...
+    timeVec, 'isRegEnabled', true, 'isJustCheck', false, 'regTol', 1e-1);
 
 %%
+%define the basis on which we project our tube
 condBasisMat = zeros(2*k+2, 2);
 condBasisMat(1:2, 1:2) = eye(2);
-
 prTubeObj = solvObj.projection(condBasisMat);
+
 plObj = smartdb.disp.RelationDataPlotter('figureGroupKeySuffFunc', ...
     @(x)sprintf('_forward_reach_set_proj%d',x));
 prTubeObj.plotByEa('r',plObj);
 hold on;
-t = 0:maxTime;
-plot3(t, V, c, 'b*');
+plot3(maxTime, v, c, 'b*');
 ylabel('V'); zlabel('c');
-title('Ellipsoidal reach tube, proj. on subspace [1 0 0 0, 0 1 0 0]');
+title('Ellipsoidal reach tube, proj. on subspace [V, c]');
 rotate3d on;
+%%
+%check whether the point belongs to the reachability tube
+[iaEllMat, timeVec] = prTubeObj.get_ia();
+x2Vec = [v, c]';
+firstEllObj = x1Vec + ellipsoid(qMat);
+iaEllMat.isinternal(x2Vec, 'u')
